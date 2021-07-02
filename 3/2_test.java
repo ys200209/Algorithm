ce2b96fb511aa5e68af29fe23e7db991e01a0264
ys200209 <lys200209@naver.com>
2021-07-03 00:15:48 +0900

    Greedy Algorithm

---
+++ 3/2_test.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.*;
+
+class Main {
+    
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(2,4,5,4,6));
+        int M = 8;
+        int K = 3;
+        int sum = 0;
+        int count = 0;
+
+        Collections.sort(list);
+        for(int i=0; i<M; i++) {
+            if(count==K) {
+                sum += list.get(list.size()-2);
+                count = 0;
+            } else {
+                sum += list.get(list.size()-1);
+                count++;
+            }
+        }
+        System.out.println(sum);
+    }
+    
+}
