3f4532945aff28d789f35134d2b853d73b09aeff
ys200209 <lys200209@naver.com>
2021-07-14 23:02:01 +0900

    Exam Greedy Algorithm

--- 11/Main11_1.java
+++ 11/Main11_1.java
@@ -1,9 +1,7 @@
 import java.util.*;
 
-import jdk.jshell.execution.FailOverExecutionControlProvider;
-
 class Main11_1 {
-    static int N, index1, index2, fail_Value, count = 0;
+    static int N, index1, index2, result = 0;
     static int[] list;
 
     public static void main(String[] args) {
@@ -19,35 +17,33 @@ class Main11_1 {
         */
 
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         list = new int[N];
-        // sc.nextLine();
-    
+
         for(int i=0; i<N; i++) {
             list[i] = sc.nextInt();
         }
-
         Arrays.sort(list);
 
-        System.out.println("list : " + Arrays.toString(list));
-        System.out.println("index1 = "+index1+", index2 = "+index2+", fail_Value = "+fail_Value+", count = " + count);
-        while(index1 < list.length && index2 < list.length) {
-            index2 = index1 + list[index1] + fail_Value;
-            System.out.println("index2 = " + index2);
-            if(list[index1] == list[index2-1]) {
-                count++;
-                System.out.println("index1 = " + index1);
-                index1 += list[index1];
-                fail_Value=0;
-            } else {
-                fail_Value++;
+        index2 = index1 + list[index1] - 1;
+        while(true) {
+            if (index2 >= list.length) {
+                break;
             }
-
-            
+            if (index2-index1+1 == list[index2]) {
+                result++;
+                if(index2 != list.length-1) { // 그룹을 묶고도 아직 길드원이 더 남았다면 index를 넘기는 과정.
+                    index1 = index2+1;
+                    index2 = index1 + list[index1] - 1;
+                } else { // 그룹을 묶었는데 마지막 인원까지 묶어서 index를 다음 길드원으로 넘길 수 없을 때.
+                    break;
+                }
+            } else {
+                index2++;
+            }
         }
-
-        System.out.println("result = " + count);
-
+        System.out.println("result = " + result);
     }
     
 }
