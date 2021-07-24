0f5792f9e1caa457520f4e833af3b4ad0f0aa57f
ys200209 <lys200209@naver.com>
2021-07-24 23:22:29 +0900

    Exam DFS/BFS Algorithm

---
+++ 13/Main13_15.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main13_15 {  
+    static int N, M, K, X;
+
+    public static void main(String[] args) {
+
+        /*
+            어떤 나라에는 1 ~ N번까지의 도시와 M개의 단방향 도로가 존재합니다. 모든 도로의 거리는 1입니다.
+            이때 특정한 도시 X로부터 출발하여 도달할 수 있는 모든 도시 중에서, 최단 거리가 정확히 K인 모든 도시의
+            번호를 출력하는 프로그램을 작성하세요. 또한 출발 도시 X에서 출발 도시 X로 가는 최단 거리는 항상
+            0이라고 가정합니다.
+            예를 들어 N = 4, K = 2, X = 1일 때 1번 도시에서 출발하여 도달할 수 있는 도시중에서,
+            최단 거리가 2인 도시는 4번 도시뿐입니다. 2번과 3번 도시의 경우, 최단 거리가 1이기 때문에 출력하지 않습니다.
+
+            도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X.
+        */
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+        X = sc.nextInt();
+
+
+
+    }
+    
+}
