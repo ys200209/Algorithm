c6fb776671198eb54d667d3fcb9a495b26e01c6f
ys200209 <lys200209@naver.com>
2021-07-31 23:30:03 +0900

    Exam DFS/BFS Algorithm

--- 12/Main12_12.java
+++ 12/Main12_12.java
@@ -1,105 +1,30 @@
 import java.util.*;
 class Main12_12 {
-    static int N, K, L, snake_size=1, x, y, dx, dy, vector, time_stamp=0, result=1;
-    static int[][] map;
-    static String[][] L_pos; // 뱀
-    static int[][] dx_dy;
-    static Queue<int[][]> queue;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
-        map = new int[N][N];
-        queue = new LinkedList<>();
-        map[0][0] = 2;
-        queue.offer(new int[][]{{0,0}});
-        dx_dy = new int[][]{{0,1},{1,0},{0,-1},{-1,0}}; // 동,남,서,북
-        vector = 0;
 
-        for(int i=0; i<K; i++) {
-            x = sc.nextInt();
-            y = sc.nextInt();
-            map[x][y] = 1;
-        }
-        L = sc.nextInt();
-        L_pos = new String[L][2];
+        /*
+            - 2020 카카오 신입 공채
 
-        for(int i=0; i<L; i++) {
-            L_pos[i][0] = sc.next();
-            L_pos[i][1] = sc.next();
-        }
-
-        for(int i=0; i<L; i++) {
-            if(!move(queue.peek(), L_pos[i][0], L_pos[i][1])) { 
-                break;
-            }
-        }
-        System.out.println(result);
-    }
-
-    public static boolean move(int[][] position, String x, String y) {
-        if(vector >= 4) { // 방향 전환
-            vector = 0;
-        } else if (vector < 0) {
-            vector = 3;
-        }
-
-        for(int i=0; i<Integer.parseInt(x)-time_stamp; i++) {
-            dx = dx_dy[vector][0];
-            dy = dx_dy[vector][1];
+            죠르디는 주택 건축사업에 뛰어들기로 결심하였습니다. 죠르디는 기둥과 보를 이용하여 벽면 구조물을
+            자동으로 세우는 로봇을 개발할 계획인데, 그에 앞서 로봇의 동작을 시뮬레이션 할 수 있는 프로그램을
+            만들고 있습니다.
+            프로그램은 2차원 가상 벽면에 기둥과 보를 이용한 구조물을 설치할 수 있는데, 기둥과 보는 길이가
+            1인 선분으로 표현되며 다음과 같은 규칙을 가지고 있다.
             
-            if(queue.peek()[0][0]+dx < 0 || queue.peek()[0][0]+dx >= N ||
-                queue.peek()[0][1]+dy < 0 || queue.peek()[0][1]+dy >= N) {
-                    return false;
-            }
+            1. 기둥은 바닥 위에 있거나 보의 한쪽 끝부분 위에 있거나, 또는 다른 기둥 위에 있어야 합니다.
+            2. 보는 한쪽 끝부분이 기둥 위에 있거나, 또는 양쪽 끝부분이 다른 보와 동시에 연결되어 있어야 합니다.
 
-            if (map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] == 1) { // Apple 발견했다면.
-                snake_size++;
-                result++;
-                map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] = 2;
-                queue.offer(new int[][]{{queue.peek()[0][0]+dx,queue.peek()[0][1]+dy}});
-            } else if (map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] == 2) { // 자신과 부딪혔다면.
-                return false;
-            } else { // 그냥 이동했다면
-                result++;
-                map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] = 2;
-                queue.offer(new int[][]{{queue.peek()[0][0]+dx,queue.peek()[0][1]+dy}});
-                int[][] pop = queue.poll();
-                map[pop[0][0]][pop[0][1]] = 0;
-            }
-        }
-        if(y.equals("D")) vector++;
-        else vector--;
+            단, 바닥은 벽면의 맨 아래 지면을 말합니다.
+            2차원 벽면은 n x n 크기 정사각 격자 형태이며, 각 격자는 1x1 크기입니다. 맨 처음 벽면은 비어있는 상태입니다.
+            기둥과 보는 격자 선의 교차점에 걸치지 않고, 격자 칸의 각 변에 정확히 일치하도록 설치할 수 있습니다.
 
-        if(x.equals(L_pos[L-1][0])) { // 마지막 인덱스까지 도착했는데도 종료되지 않았을 때.
-            dx = dx_dy[vector][0];
-            dy = dx_dy[vector][1];
-            while(true) {
-                if(queue.peek()[0][0]+dx < 0 || queue.peek()[0][0]+dx >= N ||
-                queue.peek()[0][1]+dy < 0 || queue.peek()[0][1]+dy >= N) { // 부딫혔다면
-                    break;
-                }
-                if (map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] == 1) { // Apple 발견했다면.
-                    snake_size++;
-                    result++;
-                    map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] = 2;
-                    queue.offer(new int[][]{{queue.peek()[0][0]+dx,queue.peek()[0][1]+dy}});
-                } else if (map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] == 2) { // 자신과 부딪혔다면.
-                    result++;
-                    break;
-                } else { // 그냥 이동했다면
-                    result++;
-                    map[queue.peek()[0][0]+dx][queue.peek()[0][1]+dy] = 2;
-                    queue.offer(new int[][]{{queue.peek()[0][0]+dx,queue.peek()[0][1]+dy}});
-                    int[][] pop = queue.poll();
-                    map[pop[0][0]][pop[0][1]] = 0;
-                }
-            }
-        }
-        
-        time_stamp = Integer.parseInt(x);
-        return true;
+            벽면의 크기 n, 기둥과 보를 설치하거나 삭제하는 작업이 순서대로 담긴 2차원 배열 build_frame이 매개변수로 
+            주어질 때, 모든 명령어를 수행한 후 구조물의 상태를 return 하도록 solution 함수를 완성하라.
+        */
+
+
+
     }
     
 }
