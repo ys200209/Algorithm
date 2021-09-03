b395b666c6bc9b79851cfef2bb7f7d1c3bfd1ab0
ys200209 <lys200209@naver.com>
2021-09-03 22:16:44 +0900

    Greedy Algorithm

--- 11/Main11_6.java
+++ 11/Main11_6.java
@@ -32,95 +32,48 @@ class Main11_6 {
     }
     
     public static int solution(int[] food_times, long k) {
-        int size = food_times.length;
-        int total = 0;
-        int min = 100000000;
-        int result = -1;
-        int index;
+        long size = food_times.length; // 최단 시간 음식부터 줄여나갈때 남아있는 음식의 갯수
+        long total = 0; 
+        
+
+        PriorityQueue<Food> pq = new PriorityQueue<>();
 
         for(int i=0; i<food_times.length; i++) {
-            if (food_times[i] < min) {
-                min = food_times[i]; // ex) min = 3
-            }
-            total += food_times[i];
+            pq.offer(new Food(food_times[i], i+1));
+            total = food_times[i];
         }
 
-        if (total < k) {
-            System.out.println(total + " < " + k);
+        if (total <= k) {
             return -1;
         }
 
-        System.out.println("(1) k = " + k);
+        while()
 
-        if (k - (min*size) >= 0) {
-            for(int i=0; i<food_times.length; i++) {
-                // 하나씩 빼주면 가능.
-                food_times[i] -= min;
-            }
-            k -= min*size;
-        } else {
-            min = (int) (k/size);
-            for(int i=0; i<food_times.length; i++) {
-                // 하나씩 빼주면 가능.
-                food_times[i] -= min;
-            }
-            k -= min*size;
-        }
 
-        /*for(int i=0; i<food_times.length; i++) { // [5, 0, 0, 0, 0] k=3, 이런 경우가 있기에 한바퀴만 돌면 안된다.(수정)
-            if (k == 0) break;
-            if (food_times[i] == 0) {
-                continue;
-            } else {
-                result = i+1;
-                k -= 1;
-            }
-        }*/
-
-        // System.out.println("(while) k == " + k);
-        // System.out.println(Arrays.toString(food_times));
-
-        if (k == 0) {
-            for(int i=0; i<food_times.length; i++) {
-                if (food_times[i] != 0 ) {
-                    result = i+1;
-                    return result;
-                }
-            }
-        }
-        
-        while(true) {
-            if (k == 0) break;
-            for(int i=0; i<food_times.length; i++) {
-                if (food_times[i] != 0) {
-                    k -= 1;
-                    result = i+1;
-                }
-                if (k == 0) break;
-            }
-        }
-
-        System.out.println("result = " + result);
-        if (result != -1) {
-            
-            for (int i=result; i<food_times.length+result; i++) {
-                index = i;
-                if (index >= size) {
-                    index -= size;
-                }
-                if (food_times[index] != 0) {
-                    result = index+1;
-                    break;
-                }
-            }
-            // 기존 result까지만 먹방이 진행된 후 네트워크 장애가 발생하였음을 의미한다.
-            // 따라서 네트워크 장애가 복구된 후부터 먹을 다음 음식 번호를 반환하기위해 1을 더해준다.
-        }
-
-        if (result > food_times.length) {
-            result /= food_times.length; // 한바퀴를 돌아 다시 처음 음식으로 돌아갔음을 의미함.
-        }
-        return result;
+        return -1;
     }
     
+}
+
+class Food implements Comparable<Food>{
+    private int time;
+    private int index;
+
+    public Food(int time, int index) {
+        this.time = time;
+        this.index = index;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public int getNumber() {
+        return this.index;
+    }
+
+    @Override
+    public int compareTo(Food o) {
+        return Integer.compare(this.time, o.time);
+    }
 }
