1e5da1f88f336f92c6dfbab00624e826044a98c7
ys200209 <lys200209@naver.com>
2021-09-04 22:52:41 +0900

    Implementation Algorithm

--- 12/Main12_7.java
+++ 12/Main12_7.java
@@ -1,34 +1,29 @@
 import java.util.*;
 
 class Main12_7 {
-    static int N, mid, front, back;
-    static String str;
-    static int[] list;
+    static String[] N;
+    static int front, back;
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        str = sc.nextLine();
-        mid = str.length()/2;
-        list = new int[str.length()];
+        Scanner scanner = new Scanner(System.in);
 
-        for(int i=0; i<str.length(); i++) {
-            list[i] = str.charAt(i) - '0';
+        N = scanner.next().split("");
+        front = 0;
+        back = 0;
+
+        for (int i=0; i<N.length/2; i++) {
+            front += Integer.parseInt(N[i]);
+        }
+        for(int i=N.length/2; i<N.length; i++) {
+            back += Integer.parseInt(N[i]);
         }
 
-        for(int i=0; i<mid; i++) {
-            front += list[i];
-        }
-        for(int i=mid; i<str.length(); i++) {
-            back += list[i];
-        }
-
-        if(front == back) {
+        if (front == back) {
             System.out.println("LUCKY");
         } else {
             System.out.println("READY");
         }
 
     }
-    
 }
