b5d7501c1bb586aaeae71c4e4d317c67ad845f36
ys200209 <lys200209@naver.com>
2021-09-05 18:10:42 +0900

    Implementation Algorithm

--- 12/Main12_8.java
+++ 12/Main12_8.java
@@ -1,36 +1,33 @@
 import java.util.*;
 
 class Main12_8 {
-    static String str, result_str = "";
-    static int[] list;
-    static int result_num;
-    
+    static String s, result="";
+    static int num;
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        str = sc.nextLine();
-        list = new int[str.length()];
-        Comparator<String> comp = (o1, o2) -> o1.compareTo(o2);
+        s = scanner.next();
+        num = 0;
 
-        for(int i=0; i<list.length; i++) {
-            list[i] = str.charAt(i) - '0';
-        }
-
-        System.out.println("list = " + Arrays.toString(list));
-
-        Arrays.sort(list);
-
-        for(int i=0; i<list.length; i++) {
-            if(list[i] <= 9) {
-                result_num += list[i]; 
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0; i<s.length(); i++) {
+            if (s.charAt(i) >= 'A') {
+                list.add(s.substring(i, i+1));
             } else {
-                result_str += (char)(list[i] + '0');
-                System.out.println("result_str = " + result_str);
+                num += Integer.parseInt(s.substring(i, i+1));
             }
         }
 
-        System.out.println("result = " + result_str + Integer.toString(result_num));
+        Collections.sort(list, (o1, o2) -> o1.compareTo(o2));
+
+        for(int i=0; i<list.size(); i++) {
+            result += list.get(i);
+        }
+
+        System.out.println(result + num);
+        
     }
 
 }
