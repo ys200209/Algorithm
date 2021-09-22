babcea4141d92cb99a7a3623fe9bc44bad061a40
ys200209 <lys200209@naver.com>
2021-09-22 20:35:55 +0900

    implementation Algorithm

--- 13/Main13_17.java
+++ 13/Main13_17.java
@@ -1,84 +1,34 @@
 import java.util.*;
 
 class Main13_17 {
-    static int N, K, S, X, Y;
-    static int[][] map;
-    static PriorityQueue<Virus> pq; // 우선순위 큐
-    static Queue<Virus> q; // 일반 큐
-    static Virus virus;
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+    }
         
-        map = new int[N+1][N+1];
-        pq = new PriorityQueue<>();
-        q = new LinkedList<>();
-
-        for(int i=1; i<=N; i++) { 
-            for(int j=1; j<=N; j++) {
-                map[i][j] = sc.nextInt();
-                if(map[i][j] != 0) {
-                    pq.offer(new Virus(map[i][j], new int[]{i, j})); // 바이러스가 위치한 곳의 좌표를 저장한다.
-                }
-            }
-        }
-
-        S = sc.nextInt();
-        X = sc.nextInt();
-        Y = sc.nextInt();
-
-        System.out.println("pq.size() = " + pq.size());
-        int size = pq.size();
-        for(int i=0; i<size; i++) {
-            System.out.println("값 이동 pq.peek().virus_number = " + pq.peek().virus_number);
-            q.offer(pq.poll());
-        }
-
-        System.out.println("pq.size() = " + pq.size());
-        System.out.println("q.size() = " + q.size());
-        for(int i=1; i<=S; i++) { // 특정 시간초만큼 전염시킨다.
-            for(int j=1; j<=K; j++) {
-                virus = q.poll();
-                BFS(virus.virus_position[0] - 1, virus.virus_position[1], virus.virus_number, 0);
-                BFS(virus.virus_position[0] + 1, virus.virus_position[1], virus.virus_number, 0);
-                BFS(virus.virus_position[0], virus.virus_position[1] - 1, virus.virus_number, 0);
-                BFS(virus.virus_position[0], virus.virus_position[1] + 1, virus.virus_number, 0);
-                q.offer(virus);
-            }
-            System.out.println(i + "초 경과...");
-            q = new LinkedList<>();
-            size = pq.size();
-            for(int z=0; z<size; z++) {
-                q.offer(pq.poll());
-            }
-
-            for(int a=0; a<=N; a++) {
-                System.out.println(Arrays.toString(map[a]));
-            }
-            System.out.println();
-        }
-
-        System.out.println("result = " + map[X][Y]);
-    }
-
-    public static void BFS(int dx, int dy, int number, int recursive) { // 움직일 좌표와 바이러스 숫자, 재귀 여부.
-        System.out.println("("+ dx + "," + dy+ "), number = " + number);
-        if(dx <= 0 || dx > N || dy <= 0 || dy > N) {
-            return;
-        }
-
-        if(map[dx][dy] == 0 && recursive == 0) { // 재귀가 아닌, 처음 실행될 때.
-            map[dx][dy] = number;
-            pq.offer(new Virus(number, new int[]{dx, dy}));
-        }
-
-    }
-    
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 class Virus implements Comparable<Virus> {
     public int virus_number;
     public int[] virus_position;
@@ -96,13 +46,13 @@ class Virus implements Comparable<Virus> {
         return 1;
     }
 
-    /*// 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
-    @Override
-    public int compareTo(Node other) {
-        if (this.distance < other.distance) {
-            return -1;
-        }
-        return 1;
-    }*/
+    // // 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
+    // @Override
+    // public int compareTo(Node other) {
+    //     if (this.distance < other.distance) {
+    //         return -1;
+    //     }
+    //     return 1;
+    // }
 
-}
+} */
