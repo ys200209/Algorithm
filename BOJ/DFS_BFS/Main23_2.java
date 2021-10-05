1b4e8055894b9e804c26c0ffd04975095348f6c6
ys200209 <lys200209@naver.com>
2021-10-05 16:11:53 +0900

    BOJ DFS/BFS Algorithm

--- BOJ/DFS_BFS/Main23_2.java
+++ BOJ/DFS_BFS/Main23_2.java
@@ -4,7 +4,6 @@ class Main23_2 {
     public static int N, M, x, y;
     public static int[][] map;
     public static boolean[] visited;
-    public static Queue<Integer> queue = new LinkedList<>();
     public static ArrayList<Integer> result = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -27,29 +26,21 @@ class Main23_2 {
             map[y][x] = 1;
         }
 
-        BFS();
+        DFS(1);
 
         System.out.println(result.size());
 
     }
 
-    public static void BFS() {
-        visited[1] = true;
-        queue.offer(1);
+    public static void DFS(int i) {
+        visited[i] = true;
 
-        while(!queue.isEmpty()) {
-            int temp = queue.poll();
-
-            for(int j=1; j<=N; j++) {
-                if (map[temp][j] == 1 && visited[j] == false) {
-                    visited[j] = true;
-                    queue.offer(j);
-                    result.add(j);
-                }
+        for(int j=1; j<=N; j++) {
+            if (map[i][j] == 1 && visited[j] == false) {
+                result.add(j);
+                DFS(j);
             }
         }
-
-
     }
     
 }
