88192bce9275602a26a4e301e27fb8c99763fbcd
ys200209 <lys200209@naver.com>
2021-10-06 22:11:28 +0900

    BOJ DFS/BFS Algorithm

--- BOJ/DFS_BFS/Main23_1.java
+++ BOJ/DFS_BFS/Main23_1.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
-
 class Main23_1 {
-    public static int N, M, V, x, y;
+    public static int N, M, V, X, Y;
     public static int[][] map;
     public static boolean[] visited;
     public static Queue<Integer> queue = new LinkedList<>();
@@ -20,52 +19,49 @@ class Main23_1 {
         map = new int[1001][1001];
         visited = new boolean[1001];
 
-        for(int i=0; i<M; i++) {
-            x = scanner.nextInt();
-            y = scanner.nextInt();
+        for(int i=1; i<=M; i++ ){
+            X = scanner.nextInt();
+            Y = scanner.nextInt();
 
-            map[x][y] = 1;
-            map[y][x] = 1;
+            map[X][Y] = 1;
+            map[Y][X] = 1;
+            
         }
 
-
         DFS(V);
-        System.out.println();
         visited = new boolean[1001];
+        System.out.println();
 
         BFS();
-
+        
     }
 
     public static void DFS(int i) {
         visited[i] = true;
         System.out.print(i + " ");
 
-        for(int j=1; j<=N; j++) {
-            if (map[i][j] == 1 && visited[j] == false) {
+        for(int j=1; j<=M; j++) {
+            if(map[i][j] == 1 && visited[j] == false) {
                 DFS(j);
             }
         }
     }
 
     public static void BFS() {
+        visited[V] = true;
         queue.offer(V);
         System.out.print(V + " ");
-        visited[V] = true;
-        
+
         while(!queue.isEmpty()) {
             int temp = queue.poll();
 
-            for(int j=1; j<=N; j++) {
+            for(int j=1; j<=M; j++) {
                 if (map[temp][j] == 1 && visited[j] == false) {
                     visited[j] = true;
-                    queue.offer(j);
                     System.out.print(j + " ");
+                    queue.offer(j);
                 }
             }
         }
-
-
     }
-
 }
