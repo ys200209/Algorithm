47b181ca51a811ec26303627cca380befbcfe750
ys200209 <lys200209@naver.com>
2021-10-08 23:31:48 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_5.java
+++ BOJ/Greedy/Main16_5.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 class Main16_5 {
     public static int N;
+    public static long load_sum=0;
+    public static Queue<Integer> load_queue = new LinkedList<>();
+    public static Queue<Integer> price_queue = new LinkedList<>();
+    public static PriorityQueue<Long> result = new PriorityQueue<>();
 
     public static void main(String[] args) {
 
@@ -11,6 +15,53 @@ class Main16_5 {
 
         N = scanner.nextInt();
 
+        for(int i=0; i<N-1; i++) { // load
+            load_queue.offer(scanner.nextInt());
+            load_sum += load_queue.peek();
+        }
+
+        for(int i=0; i<N; i++) { // oil_price
+            price_queue.offer(scanner.nextInt());
+        }
+
+        
+
+        System.out.println(BFS());
+
+    }
+
+    public static long BFS() {
+
+        result.offer((long)(price_queue.peek() * load_queue.peek()));
+
+        while(load_queue.size() != 1) {
+            
+            //   [2, 3, 1]
+            // [5, 2, 4, 1]
+            long now = result.poll();
+
+            long price = price_queue.poll();
+
+            long front = price * load_sum;
+            
+            // now = price * load_queue.peek();
+            load_sum -= load_queue.peek();
+
+            long back = now + (price_queue.peek() * load_sum);
+
+            System.out.println("front : " + front);
+            System.out.println("back : " + back);
+
+            if (front >= back) {
+                result.offer(now + price_queue.peek() * load_queue.poll());
+                
+                continue;
+            }
+
+            return front;
+        }
+
+        return result.poll();
 
     }
 }
