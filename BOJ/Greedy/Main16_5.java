3da30b6ba218b937251e39e2c739e6c1b552a382
ys200209 <lys200209@naver.com>
2021-10-08 22:29:59 +0900

    BOJ Greedy Algorithm

---
+++ BOJ/Greedy/Main16_5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main16_5 {
+    public static int N;
+
+    public static void main(String[] args) {
+
+        // 백준 온라인 저지 Greedy(16)의 5번
+        
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+
+    }
+}
