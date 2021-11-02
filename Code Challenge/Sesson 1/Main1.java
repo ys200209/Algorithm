3b0916b9b2125c624b9b30fc5546da39cde32037
ys200209 <lys200209@naver.com>
2021-11-02 21:16:57 +0900

    Code Challenge Sesson 1

---
+++ Code Challenge/Sesson 1/Main1.java
@@ -0,0 +1,19 @@
+class Main1 {
+    
+    public static void main(String[] args) {
+
+        System.out.println(solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2}));
+
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for(int i=0; i<a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+
+}
