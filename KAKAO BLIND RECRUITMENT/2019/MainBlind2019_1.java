6067492580da54f160d9c9bb5664ef49586354ef
ys200209 <lys200209@naver.com>
2021-11-02 21:16:38 +0900

    KAKAO BLIND 2019

--- KAKAO BLIND RECRUITMENT/2019/MainBlind2019_1.java
+++ KAKAO BLIND RECRUITMENT/2019/MainBlind2019_1.java
@@ -4,7 +4,7 @@ class MainBlind2019_1 {
 
     public static void main(String[] args) {
 
-        // 오픈 채팅방 (2)
+        // 2019 카카오 블라인드 테스트. 오픈 채팅방 (2)
 
         System.out.println(Arrays.toString(solution(new String[] {
             "Enter uid1234 Muzi", 
@@ -22,35 +22,36 @@ class MainBlind2019_1 {
 
     public static String[] solution(String[] record) {
         Map<String, String> map = new HashMap<>();
+        String action = null;
         String userID = null;
         String userNick = null;
-        int count = record.length;
+        ArrayList<String> list = new ArrayList<>();
 
         for (int i=0; i<record.length; i++) {
-            if (!record[i].split(" ")[0].equals("Leave")) {
+            action = record[i].split(" ")[0];
+            if (!action.equals("Leave")) {
                 userID = record[i].split(" ")[1];
                 userNick = record[i].split(" ")[2];
                 map.put(userID, userNick);
             }
-            if (record[i].split(" ")[0].equals("Change")) {
-                count--;
+        }
+
+        for(int i=0; i<record.length; i++) {
+            action = record[i].split(" ")[0];
+            if (action.equals("Enter")) {
+                userID = record[i].split(" ")[1];
+                list.add(map.get(userID) + "님이 들어왔습니다.");
+
+            } else if (action.equals("Leave")) {
+                userID = record[i].split(" ")[1];
+                list.add(map.get(userID) + "님이 나갔습니다.");
             }
         }
 
-        String[] answer = new String[count];
-        count=0;
+        String[] answer = new String[list.size()];
 
-        for(int i=0; i<record.length; i++) {
-            if (record[i].split(" ")[0].equals("Enter")) {
-                userID = record[i].split(" ")[1];
-                answer[count] = map.get(userID) + "님이 들어왔습니다.";
-
-            } else if (record[i].split(" ")[0].equals("Leave")) {
-                userID = record[i].split(" ")[1];
-                answer[count] = map.get(userID) + "님이 나갔습니다.";
-            }
-
-            count ++;
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
 
