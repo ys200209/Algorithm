ae16bf72b379ab2f4258e2c4b98967022ac123ce
ys200209 <lys200209@naver.com>
2021-11-05 16:37:26 +0900

    DFS/BFS Algorithm

--- skill/DFS_BFS/Main1.java
+++ skill/DFS_BFS/Main1.java
@@ -3,67 +3,31 @@ package DFS_BFS;
 import java.util.*;
 
 public class Main1 {
-    public static int N, M, V, x, y;
-    public static int[][] map;
-    public static boolean[] visited;
-
-
+    public static int answer = 0;
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-        V = scanner.nextInt();
-
-        map = new int[1001][1001];
-        visited = new boolean[1001];
-
-        for(int i=0; i<M; i++) {
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            map[x][y] = 1;
-            map[y][x] = 1;
-        }
-
-        DFS(V);
-
-        visited = new boolean[1001];
-        System.out.println();
-
-        BFS();
+        // 타겟 넘버
+        System.out.println(solution(new int[]{1, 1, 1, 1, 1}, 3)); // 5
 
     }
+    
+    public static int solution(int[] numbers, int target) {
 
-    public static void DFS(int i) {
-        visited[i] = true;
-        System.out.print(i + " ");
+        DFS(numbers, 0, 0, target);
 
-        for(int j=1; j<=N; j++) {
-            if (map[i][j] == 1 && visited[j] == false) {
-                DFS(j);
-            }
-        }
+        return answer;
     }
 
-    public static void BFS() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(V);
-        visited[V] = true;
-
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            System.out.print(node + " ");
-
-            for(int j=1; j<=N; j++) {
-                if (map[node][j] == 1 && visited[j] == false) {
-                    queue.offer(j);
-                    visited[j] = true;
-                }
+    public static void DFS(int[] numbers, int sum, int index, int target) {
+        if (index >= numbers.length) {
+            if (sum == target) {
+                answer++;
             }
-
+            return;
         }
 
+        DFS(numbers, sum-numbers[index], index+1, target);
+        DFS(numbers, sum+numbers[index], index+1, target);
+        return;
     }
 
 }
