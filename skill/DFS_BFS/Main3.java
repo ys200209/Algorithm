3fb75de58afb6ab825a25ffe8da4c929741c313e
ys200209 <lys200209@naver.com>
2021-11-06 20:56:53 +0900

    DFS/BFS Algorithm

---
+++ skill/DFS_BFS/Main3.java
@@ -0,0 +1,20 @@
+package DFS_BFS;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        // 단어 변환
+        System.out.println(solution("hit", "cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"})); // 4
+        System.out.println(solution("hit", "cog", new String[] {"hot", "dot", "dog", "lot", "log"})); // 0
+
+
+
+    }
+
+    public static int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
