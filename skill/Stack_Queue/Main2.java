6cea86ddbf41b438cb68fc1e2c1fb9c9b1362e9f
ys200209 <lys200209@naver.com>
2021-11-08 22:33:25 +0900

    Stack Queue Algorithm

--- skill/Stack_Queue/Main2.java
+++ skill/Stack_Queue/Main2.java
@@ -3,13 +3,13 @@ package Stack_Queue;
 import java.util.*;
 
 public class Main2 {
-    public static Queue<Printer> queue = new PriorityQueue<>();
+    public static Queue<Printer> queue = new LinkedList<>();
     public static Queue<Integer> pri_queue = new PriorityQueue<>(Collections.reverseOrder());
 
     public static void main(String[] args) {
 
         // 프린터
-        //System.out.println(solution(new int[]{2, 1, 3, 2}, 2)); // 1
+        // System.out.println(solution(new int[]{2, 1, 3, 2}, 2)); // 1
         System.out.println(solution(new int[]{1, 1, 9, 1, 1, 1}, 0)); // 5
 
     }
@@ -22,22 +22,27 @@ public class Main2 {
             pri_queue.offer(priorities[i]);
         }
 
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()) { // 큐가 빌때까지 추출
             
-        }
+            if (queue.peek().getImp() == pri_queue.peek()) { // 만약 추출할 프린트의 우선순위가 최고순위라면
+                answer ++;
+                if (queue.peek().getIndex() == location) { // 추출할 프린트의 순서가 location 번째로 출력된다면 리턴
+                    return answer;
+                }
+                queue.poll(); // 추출하라
+                pri_queue.poll(); // 추출하라
+            } else {
+                queue.offer(queue.poll()); // 추출할 프린트의 우선순위가 상대적으로 낮다면 다시 큐의 마지막에 삽입
+            }
 
-        while(!pri_queue.isEmpty()) {
-            //System.out.println(queue.poll().getIndex());
-            System.out.println(pri_queue.poll());
         }
         
-
         return answer;
     }
     
 }
 
-class Printer implements Comparable<Printer>{
+class Printer {
     private int index;
     private int imp;
 
@@ -53,18 +58,5 @@ class Printer implements Comparable<Printer>{
     public int getImp() {
         return imp;
     }
-
-    @Override
-    public int compareTo(Printer o) {
-        if (this.imp > o.imp) {
-            return -1;
-        }
-        return 1;
-    }
-
-
     
-
-    
-
 }
