eed3072f79ebb4ab29f19696aee80d954f8e254a
ys200209 <lys200209@naver.com>
2021-11-09 15:07:55 +0900

    BOJ PriorityQueue

---
+++ BOJ/Heap/Main22_2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main22_2 {
+    public static Queue<Integer> queue = new PriorityQueue<>();
+    public static int N, x;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        // 백준 온라인 저지 우선순위 큐(22)의 2번
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        for(int i=0; i<N; i++) {
+            x = scanner.nextInt();
+
+            if (x == 0) {
+                if (queue.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(queue.poll()+"\n");
+                }
+            } else {
+                queue.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
