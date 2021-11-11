698c7ec13b9d0c2f3255ef0cf0ec3d3abb6f3600
ys200209 <lys200209@naver.com>
2021-11-11 18:12:23 +0900

    PriorityQueue Algorithm

--- skill/Heap/Main3.java
+++ skill/Heap/Main3.java
@@ -3,26 +3,82 @@ package Heap;
 import java.util.*;
 
 public class Main3 {
-    Deque<Integer> deq = new ArrayDeque<>();
-    Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-    queue = new PriorityQueue<>(queue, Collections.reverseOrder());
-    
+    public static Queue<Integer> queue = new PriorityQueue<>();
+    public static Queue<Integer> readyQueue = new PriorityQueue<>(Collections.reverseOrder());
 
     public static void main(String[] args) {
 
-        System.out.println(solution(new String[]{"I 16","D 1"})); // [0, 0]
-        System.out.println(solution(new String[]{"I 7","I 5","I -5","D -1"})); // [7, 5]
+        // 이중 우선순위 큐 (2)
+        
+        //System.out.println(Arrays.toString(solution(new String[]{"I 16","D 1"}))); // [0, 0]
+        //System.out.println(Arrays.toString(solution(new String[]{"I 7","I 5","I -5","D -1"}))); // [7, 5]
+        System.out.println(Arrays.toString(solution(
+            new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"}
+            ))); // [333, -45]
+        
+        // [-45, 45, 333]
 
     }
 
     public static int[] solution(String[] operations) {
-        int[] answer = {};
+        int[] answer = new int[2];
         String command = null;
         int number = 0;
 
+        for(int i=0; i<operations.length; i++) {
+            command = operations[i].split(" ")[0];
+            number = Integer.parseInt(operations[i].split(" ")[1]);
 
-        
+            if (command.equals("I")) {
+                if (!queue.isEmpty()) {
+                    queue.offer(number);
+                } else {
+                    readyQueue.offer(number);
+                }
+                System.out.println("I");
+            } else {
+                if (number == 1) {
+                    while(!queue.isEmpty()) {
+                        readyQueue.offer(queue.poll());
+                    }
+                    readyQueue.poll();
+                } else {
+                    while(!readyQueue.isEmpty()) {
+                        queue.offer(readyQueue.poll());
+                    }
+                    queue.poll();
+                }
+            }
+        }
 
+        if (queue.isEmpty() && readyQueue.isEmpty()) {
+            return new int[]{0, 0};
+        }
+
+        System.out.println("- queue.size() = " + queue.size());
+        System.out.println("- readyQueue.size() = " + readyQueue.size());
+
+        if (queue.size() == 1 || readyQueue.size() == 1) {
+            if (queue.isEmpty()) {
+                int ret = readyQueue.poll();
+                return new int[]{ret, ret};
+            } else {
+                int ret = queue.poll();
+                return new int[]{ret, ret};
+            }
+        }
+
+        if (queue.isEmpty()) {
+            answer[0] = readyQueue.poll();
+            while(!readyQueue.isEmpty()) {
+                answer[1] = readyQueue.poll();
+            }
+        } else {
+            answer[1] = queue.poll();
+            while(!queue.isEmpty()) {
+                answer[0] = queue.poll();
+            }
+        }
         return answer;
     }
     
