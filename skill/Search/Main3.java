adc5b8bd3e916570078fa830db64198f1f1e8058
ys200209 <lys200209@naver.com>
2021-11-12 23:10:52 +0900

    Search Algorithm

---
+++ skill/Search/Main3.java
@@ -0,0 +1,22 @@
+package Search;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        // 카펫 (2)
+        System.out.println(solution(10, 2)); // [4, 3]
+        System.out.println(solution(8, 1)); // [3, 3]
+        System.out.println(solution(24, 24)); // [8, 6]
+
+    }
+
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+        
+
+        return answer;
+    }
+    
+}
