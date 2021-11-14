361d1f178d1642dd7be60a966ecc20ffba000200
ys200209 <lys200209@naver.com>
2021-11-14 21:42:15 +0900

    Kakao WInter

--- 2019 Kakao Winter/Main2019_1.java
+++ 2019 Kakao Winter/Main2019_1.java
@@ -6,6 +6,8 @@ public class Main2019_1 {
 
         System.out.println(solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, 
                 new int[]{1,5,3,5,1,2,1,4})); // 4
+        
+                
 
     }
 
