9af05a5a729364ad9822806c09dcb425ebc77ddd
ys200209 <lys200209@naver.com>
2021-11-14 21:41:32 +0900

    PriorityQueue Algorithm

---
+++ BOJ/Heap/Main22_4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main22_4 {
+    public static Queue<Integer> queue1 = new PriorityQueue<>();
+    public static Queue<Integer> queue2 = new PriorityQueue<>();
+    public static int N, x;
+    public static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) {
+
+        // 백준 온라인 저지 우선순위 큐(22)의 4번
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        for(int i=0; i<N; i++) {
+            x = scanner.nextInt();
+
+            // queue.offer(x);
+        }
+
+
+
+
+    }
+}
+
