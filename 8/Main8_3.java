1e47dc254824d59631bea24ae5d3ea50afa19ab9
ys200209 <lys200209@naver.com>
2021-11-17 23:44:18 +0900

    Dynamic Programming Algorithm

--- 8/Main8_3.java
+++ 8/Main8_3.java
@@ -1,30 +1,42 @@
 import java.util.*;
 
 class Main8_3 {
-    static int N;
-    static int[] list;
+    public static int N, max;
+    public static int[] store, d;
+    public static boolean[] visited;
+
     public static void main(String[] args) {
 
         /*
             4
             1 3 1 5
         */
-
-        Scanner sc = new Scanner(System.in);
         
-        N = sc.nextInt();
-        list = new int[N];
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        store = new int[N];
+        visited = new boolean[N];
+        d = new int[100];
 
         for(int i=0; i<N; i++) {
-            list[i] = sc.nextInt();
+            store[i] = scanner.nextInt();
         }
 
-        for(int i=0; i<N-3; i+=2) {
-            if(list[0]+list[1] > list[1]+list[2]) {
-                
-            }
-        }
+        dynamic(store);
 
+        System.out.println("max = " + d[N-1]);
+        System.out.println(Arrays.toString(d));
+
+    }
+
+    public static void dynamic(int[] store) {
+        d[0] = store[0];
+        d[1] = Math.max(store[0], store[1]);
+
+        for(int i=2; i<store.length; i++) {
+            d[i] = Math.max(d[i-2]+store[i], d[i+1]);
+        }
     }
 
 }
