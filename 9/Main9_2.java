436b4e1a55e9e7e9d9df95152ac889d8f3cebb25
ys200209 <lys200209@naver.com>
2021-11-22 23:49:17 +0900

    Dijkstra Algorithm

---
+++ 9/Main9_2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main9_2 {
+    public static final int INF = (int)1e9;
+    public static int n, m, start; // ()
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
