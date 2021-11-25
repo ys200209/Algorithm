539758f2778fef0254c3e699f846deb376161f13
ys200209 <lys200209@naver.com>
2021-11-26 00:41:45 +0900

    Dijkstra Algorithm

--- 9/Main92.java
+++ 9/Main92.java
@@ -1,35 +1,37 @@
-
 import java.util.*;
 
 public class Main92 {
-	public static final int INF = (int)1e9;
-	public static PriorityQueue<Node1> pq = new PriorityQueue<>();
+	public static final int INF = (int) 1e9;
 	public static int n, m, start;
 	public static ArrayList<ArrayList<Node1>> graph = new ArrayList<>();
 	public static int[] d = new int[100001];
+	public static Queue<Node1> pq = new PriorityQueue<>();
+	
 	
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
-		m = scanner.nextInt();
-		start = scanner.nextInt();
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		start = sc.nextInt();
 		
 		for(int i=0; i<=n; i++) {
 			graph.add(new ArrayList<Node1>());
 		}
 		
 		for(int i=0; i<m; i++) {
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			int c = scanner.nextInt();
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
 			
 			graph.get(a).add(new Node1(b, c));
 		}
 		
-		Arrays.fill(d, INF);;
+		Arrays.fill(d, INF);
 		
 		dijkstra(start);
+		
 		
 	}
 	
@@ -40,22 +42,21 @@ public class Main92 {
 		
 		while(!pq.isEmpty()) {
 			Node1 node = pq.poll();
-			int index = node.getIndex();
+			int now = node.getIndex();
 			int distance = node.getDistance();
 			
-			if (d[index] < distance) continue;
+			if (d[now] < distance) continue;
 			
-			for(int i=0; i<graph.get(index).size(); i++) {
-				int cost = d[i] + graph.get(index).get(i).getDistance();
-				if (d[graph.get(index).get(i).getIndex()] > cost) {
-					d[graph.get(index).get(i).getIndex()] = cost;
-					pq.offer(new Node1(graph.get(index).get(i).getIndex(), cost));
+			for(int i=0; i<graph.get(now).size(); i++) {
+				int cost = d[now] + graph.get(now).get(i).getDistance();
+				
+				if (cost < d[graph.get(now).get(i).getIndex()]) {
+					d[graph.get(now).get(i).getIndex()] = cost;
+					pq.offer(new Node1(graph.get(now).get(i).getIndex(), cost));
 				}
 			}
 		}
-		
 	}
-	
 }
 
 class Node1 implements Comparable<Node1>{
@@ -63,7 +64,7 @@ class Node1 implements Comparable<Node1>{
 	private int index;
 	private int distance;
 	
-	public Node1 (int index, int distance) {
+	public Node1(int index, int distance) {
 		this.index = index;
 		this.distance = distance;
 	}
