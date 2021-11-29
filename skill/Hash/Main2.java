2c57961cc5eb26d9d2e1407e7eff64ed3695584e
ys200209 <lys200209@naver.com>
2021-11-29 23:45:02 +0900

    Semaphore Reader/Writer Example

--- skill/Hash/Main2.java
+++ skill/Hash/Main2.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public class Main2 {
+    public static Map<String, Integer> map = new HashMap<>();
+    public static Set<String> set = new TreeSet<>();
     
     public static void main(String[] args) {
 
@@ -11,11 +13,18 @@ public class Main2 {
     }
 
     public static boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
-        System.out.println(Arrays.toString(phone_book));        
+        Arrays.sort(phone_book);  
 
-        return answer;
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=i+1; j<phone_book.length; j++) {
+                if (phone_book[j].startsWith(phone_book[i]) || phone_book[i].startsWith(phone_book[j])) {
+                    return false;
+                }
+            }
+        }
+        
+
+        return true;
     }
 
 }
