7edaffcbf4841635a77062f98d50d4cc1b19180b
ys200209 <lys200209@naver.com>
2021-12-27 13:21:19 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_5.java
+++ BOJ/Greedy/Main16_5.java
@@ -3,81 +3,44 @@ import java.io.*;
 
 class Main16_5 {
     public static int N;
-    public static int[] cities, price;
-    public static boolean[] visited;
-    public static long sum = 0, price_sum=0, result = (long)1e18;
-
+    public static long result=0;
+    public static int[] distance, amounts;
+    
     public static void main(String[] args) throws IOException {
 
-        // 백준 온라인 저지 Greedy(16)의 5번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        /*N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        cities = new int[N];
-        visited = new boolean[N];
-        price = new int[N-1];
+        distance = new int[N-1];
+        amounts = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i=0; i<N-1; i++) {
-            price[i] = Integer.parseInt(st.nextToken());
-            price_sum += price[i];
+        int k=0;
+        while(st.hasMoreTokens()) {
+            distance[k] = Integer.parseInt(st.nextToken());
+            k++;
         }
 
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i=0; i<N; i++) {
-            cities[i] = Integer.parseInt(st.nextToken());
+        k=0;
+        while(st.hasMoreTokens()) {
+            amounts[k] = Integer.parseInt(st.nextToken());
+            k++;
         }
 
-        DFS(0);
-
-
-        System.out.println(result);*/
-
-        br.readLine();
-
-        String[] dis = br.readLine().split(" ");
-        String[] amounts = br.readLine().split(" ");
-
-        long min = Long.parseLong(amounts[0]), tot = 0;
+        long amo = amounts[0];
         
-        for (int i = 0; i < dis.length; i++) {
-            long d = Long.parseLong(dis[i]);
-            long a = Long.parseLong(amounts[i]);
+        for(int i=0; i<distance.length; i++) {
 
-            if(a < min) min = a;
+            if (amo > amounts[i]) amo = amounts[i];
 
-            tot += (min * d);
+            long dis = distance[i];
+
+            result += dis * amo;
         }
 
-        System.out.println(tot);
-    
-    }
+        System.out.println(result);
 
-    public static void DFS(int count) {
-        
-        if (count == N-1 || price_sum == 0) {
-            if (result > sum) 
-            result = sum;
-            return;
-        }
-
-        for(int i=0; i<cities.length-1; i++) {
-            if (!visited[i]) {
-                for(int k=price[count]; k<=price_sum; k++) {
-                    if (k != price[count] && k != price_sum) return;
-                    sum += cities[count] * k;
-                    price_sum -= k;
-                    visited[count] = true;  
-                    count += 1;
-                    DFS(count);
-                    count -= 1;
-                    visited[count] = false;
-                    price_sum += k;
-                    sum -= cities[count] * k;
-                }       
-            }
-        }
     }
 
 }
