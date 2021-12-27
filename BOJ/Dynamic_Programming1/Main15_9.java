ff427485017c2a7c63c70ab7cc90bf8b1c89bc70
ys200209 <lys200209@naver.com>
2021-12-27 22:48:41 +0900

    BOJ Dynamic Programming

---
+++ BOJ/Dynamic_Programming1/Main15_9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main15_9 {
+    public static int N;
+    public static int[] d;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        N  = scanner.nextInt();
+        d = new int[101];
+
+        d[0] = 0;
+        d[9] = 1;
+
+    }
+    
+}
