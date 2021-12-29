b4027b975a78097d83592a900b26323aea20614f
ys200209 <lys200209@naver.com>
2021-12-29 16:24:32 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_9.java
+++ BOJ/Dynamic_Programming1/Main15_9.java
@@ -1,33 +1,32 @@
 import java.util.*;
 
 public class Main15_9 {
-    public static int N, result=0;
-    public static int[][] d;
-
+    public static int N;
+    public static long result=0;
+    public static long[][] d;
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        N  = scanner.nextInt();
-        d = new int[N+1][10];
         
-        d[1][0] = 0;
+        N = scanner.nextInt();
+        d = new long[N+1][10];
+
         for(int i=1; i<10; i++) {
             d[1][i] = 1;
         }
 
         for(int i=2; i<=N; i++) {
             for(int j=0; j<10; j++) {
-                if (j == 0) {
-                    d[i][j] = d[i-1][1];
-                } else if (j == 9) {
-                    d[i][j] = d[i-1][8];
-                } else {
-                    d[i][j] = d[i-1][j-1] + d[i-1][j+1];
+                if (j == 0) d[i][j] = (d[i-1][1]) % 1000000000;
+                else if (j == 9) d[i][j] = (d[i-1][8]) % 1000000000;
+                else {
+                    d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % 1000000000;
                 }
             }
         }
 
-        for(int i=0; i<N+1; i++) {
+        for(int i=0; i<=N; i++) {
             System.out.println(Arrays.toString(d[i]));
         }
 
@@ -35,11 +34,8 @@ public class Main15_9 {
             result += d[N][i];
         }
 
-        System.out.println(result);
-
-
-
+        System.out.println(result%1000000000);
 
     }
-    
+
 }
