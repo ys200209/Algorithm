f91e81070c9ee6e89d3d353a6df3ac81717caa74
ys200209 <lys200209@naver.com>
2021-12-30 15:43:29 +0900

    Greedy Algorithm

--- 11/Main11_2.java
+++ 11/Main11_2.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 class Main11_2 {
-    static String[] S;
-    static String[] arr;
-    static int num, total;
+    public static int result=0;
+    public static String S;
+    
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        S = scanner.next().split("");
-        total = Integer.parseInt(S[0]);
-        
-        for(int i=1; i<S.length; i++) {
-            if( total == 0 || total == 1 || Integer.parseInt(S[i]) == 0 || Integer.parseInt(S[i]) == 1) {
-                total += Integer.parseInt(S[i]);
+        S = scanner.nextLine();
+
+        int result = S.charAt(0) - '0';
+
+        for(int i=1; i<S.length(); i++) {
+            if (S.charAt(i) - '0' <= 1 || result <= 1) {
+                result += S.charAt(i) - '0';
             } else {
-                total *= Integer.parseInt(S[i]);
+                result *= S.charAt(i) - '0';
             }
         }
 
-        System.out.println(total);
-
+        System.out.println(result);
+        
     }
 
 }
