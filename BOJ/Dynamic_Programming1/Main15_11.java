93220d617bbc1f4ec524bb1765b5840f7c60e15c
ys200209 <lys200209@naver.com>
2022-01-01 22:39:53 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_11.java
+++ BOJ/Dynamic_Programming1/Main15_11.java
@@ -1,38 +1,39 @@
 import java.util.*;
 
 public class Main15_11 {
-    public static int N, result=1;
-    public static int[] A, d;
-
+    static int N, result;
+    static int[] A, d;
+ 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
 
+        N = scanner.nextInt();
         A = new int[N+1];
         d = new int[N+1];
 
-        d[1] = 1;
-
         for(int i=1; i<=N; i++) {
             A[i] = scanner.nextInt();
+            d[i] = 1;
+        }
+        
+
+        if (N == 1) {
+            System.out.println("1");
+            return;
         }
 
-        for(int i=2; i<=N; i++) {
-            for(int j=i-1; j>=1; j--) {
-                if (A[j] < A[i]) {
-                    d[i] = Math.max(d[i], d[j] + 1);
-                }
+        for(int i=1; i<=N; i++) {
+            for(int j=i+1; j<=N; j++) {
+                if (A[i] < A[j] && d[j] < d[i]+1) {
+                    d[j] = d[i]+1;
+                } 
             }
-            if (d[i] == 0 ) {
-                d[i] = 1;
-            }
-            result = Math.max(result, d[i]);
+
         }
 
         System.out.println(Arrays.toString(d));
-        System.out.println(result);
 
     }
-    
+
 }
