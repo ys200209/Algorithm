1ede0bba9c54c5947ac579bfa319d0aa92303b61
ys200209 <lys200209@naver.com>
2022-01-05 13:08:18 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_5.java
+++ BOJ/Dynamic_Programming1/Main15_5.java
@@ -1,43 +1,52 @@
 import java.util.*;
 
 public class Main15_5 {
-    public static int N, result;
-    public static int[][] cost, d;
+    static int N, result=(int)1e9;
+    static int[][] cost, d;
+    
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         N = scanner.nextInt();
+        cost = new int[N+1][3];
+        d = new int[N+1][3];
 
-        cost = new int[N][N];
-        d = new int[1001][3];
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<3; j++) {
-                cost[i][j] = scanner.nextInt();
-            }
+        for(int i=1; i<=N; i++) {
+            cost[i][0] = scanner.nextInt();
+            cost[i][1] = scanner.nextInt();
+            cost[i][2] = scanner.nextInt();
         }
 
-        d[0][0] = cost[0][0];
-        d[0][1] = cost[0][1];
-        d[0][2] = cost[0][2];
+        TopDown(N);
 
-        int index_a, index_b;
-        for(int i=1; i<N; i++) {
-            for(int j=0; j<3; j++) {
-                index_a = j+1 >= 3 ? j+1-3 : j+1;
-                index_b = j+2 >= 3 ? j+2-3 : j+2;
-                d[i][j] = Math.min(d[i-1][index_a] + cost[i][j], d[i-1][index_b] + cost[i][j]);
-            }
-        }
-
-        result = d[N-1][0];
         for(int i=0; i<3; i++) {
-            if (result > d[N-1][i]) result = d[N-1][i];
+            result = Math.min(result, d[N][i]);
         }
-        
-        System.out.println(result);
 
+        System.out.println(result);
+        
     }
-    
+
+    public static int[] TopDown(int index) {
+        if (index == 0) return d[0];
+
+        for(int i=0; i<3; i++) {
+            if (i == 0) {
+                d[index][0] = d[index][0] == 0 ? Math.min(TopDown(index-1)[1] + cost[index][0], 
+                        TopDown(index-1)[2] + cost[index][0]) : d[index][0];
+                        
+            } else if (i == 1) {
+                d[index][1] = d[index][1] == 0 ? Math.min(TopDown(index-1)[0] + cost[index][1], 
+                        TopDown(index-1)[2] + cost[index][1]) : d[index][1];
+            } else {
+                d[index][2] = d[index][2] == 0 ? Math.min(TopDown(index-1)[0] + cost[index][2],
+                        TopDown(index-1)[1] + cost[index][2]) : d[index][2];
+            }
+        }
+
+        return d[index];
+    }
+
 }
