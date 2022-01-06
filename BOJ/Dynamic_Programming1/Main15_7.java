b91d76fdad4dfb129888a7ea658e346f5676310c
ys200209 <lys200209@naver.com>
2022-01-06 20:25:38 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_7.java
+++ BOJ/Dynamic_Programming1/Main15_7.java
@@ -2,36 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main15_7 {
-    public static int N;
-    public static int[] stairs, d;
+    static int N, result=0;
+    static int[] stairs, d;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
         stairs = new int[N+1];
         d = new int[N+1];
-
+        
         for(int i=1; i<=N; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
 
-        // d[0] = stairs[0];
-        d[1] = stairs[1];
+        TopDown(N);
 
-        if (N >= 2) {
-            d[2] = stairs[1] + stairs[2];
-        } 
-
-        for(int i=3; i<=N; i++) {
-            d[i] = Math.max(d[i-3]+stairs[i-1], d[i-2]) + stairs[i];
+        for(int i=0; i<=N; i++) {
+            result = Math.max(result, d[i]);
         }
 
+        System.out.println(Arrays.toString(stairs));
 
-        // System.out.println(Arrays.toString(d));
+        System.out.println(Arrays.toString(d));
 
-        System.out.println(d[N]);
+        System.out.println(result);
 
+    }
+
+    public static int TopDown(int index) {
+        if (index == 0) return 0;
+        if (index == 1) return d[index] = stairs[index];
+        if (index == 2) return d[index] = stairs[index] + stairs[index-1];
+
+        if (d[index] == 0) {
+            return d[index] = Math.max(TopDown(index-2), TopDown(index-3) + stairs[index-1]) + stairs[index];
+        }
+
+        return d[index];
     }
     
 }
