222497e45a566027baf21b423cc52219c7a21975
ys200209 <lys200209@naver.com>
2022-01-09 15:51:55 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_12.java
+++ BOJ/Dynamic_Programming1/Main15_12.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class Main15_12 {
-    public static int N, result=0;
-    public static int[] A, inc_d, dec_d;
-
+    static int N, result=0;
+    static int[] A, inc_d, dec_d;
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -15,46 +15,58 @@ public class Main15_12 {
 
         for(int i=1; i<=N; i++) {
             A[i] = scanner.nextInt();
-            inc_d[i] = 1;
-            dec_d[i] = 1;
         }
 
-        if (N == 1) {
-            System.out.println("1");
-            return;
+        inc_d[1] = 1;
+        dec_d[N] = 1;
+        
+        for(int i=2; i<=N; i++) {
+            increaseTopDown(i);
+        }
+
+        for(int i=N-1; i>=1; i--) {
+            decreaseTopDown(i);
         }
         
         for(int i=1; i<=N; i++) {
-            for(int j=i+1; j<=N; j++) {
-                if (A[i] < A[j] && inc_d[j] < inc_d[i]+1) {
-                    inc_d[j] = inc_d[i]+1;
-                } else if (A[i] > A[j]) {
-                    
+            result = Math.max(result, inc_d[i] + dec_d[i]);
+        }
+
+
+        // System.out.println("inc : " + Arrays.toString(inc_d));
+        // System.out.println("dec : " + Arrays.toString(dec_d));
+
+        System.out.println(result-1);
+    }
+
+    public static int increaseTopDown(int index) {
+        if (index == 1) return 1;
+
+        if (inc_d[index] == 0) {
+            for(int i=index - 1; i>0; i--) {
+                if (A[i] < A[index]) {
+                    inc_d[index] = Math.max(inc_d[index], increaseTopDown(i) + 1);
+                } 
+            }
+            if (inc_d[index] == 0) inc_d[index] = 1;
+        }
+
+        return inc_d[index];
+    }
+
+    public static int decreaseTopDown(int index) {
+        if (index == N) return 1;
+
+        if (dec_d[index] == 0) {
+            for(int i=index+1; i<=N; i++) {
+                if (A[index] > A[i]) {
+                    dec_d[index] = Math.max(dec_d[index], dec_d[i]+1);
                 }
             }
-        }
-        
-        //for(int i=N; i>=1; i++) {
-            
-        //}
-
-        for(int j=N-1; j>=1; j--) {
-            if (A[j] > A[j+1] && dec_d[j] < dec_d[j+1] + 1) {
-                dec_d[j] = dec_d[j+1] + 1;
-            }
+            if (dec_d[index] == 0) dec_d[index] = 1;
         }
 
-        for(int i=2; i<=N; i++) {
-            if (A[i-1] > A[i]) {
-                result = Math.max(result, inc_d[i-1] + dec_d[i]);
-            } 
-        }
-
-        System.out.println("inc_d = " + Arrays.toString(inc_d));
-        System.out.println("dec_d = " + Arrays.toString(dec_d));
-
-        System.out.println(result);
-
+        return dec_d[index];
     }
-    
+
 }
