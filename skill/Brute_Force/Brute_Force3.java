4e548c25bfebd02a00bc117fb9f1db9cc3cada6f
ys200209 <lys200209@naver.com>
2022-01-10 22:12:13 +0900

    Brute_Force Algorithm

---
+++ skill/Brute_Force/Brute_Force3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Brute_Force3 {
+    static int brown, yellow, sq;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        brown = scanner.nextInt();
+        yellow = scanner.nextInt();
+
+        sq = brown + yellow;
+
+        
+
+    }
+    
+}
