6c1e247af388dfc227d04942561eb581e4b4ab58
ys200209 <lys200209@naver.com>
2022-01-11 23:22:03 +0900

    BOJ String Algorithm

---
+++ BOJ/String_Algorithm/Main7_1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main7_1 {
+    static int S;
+
+    public static void main(String[] args) throws IOException {
+        
+        S = System.in.read();
+
+        System.out.println(S);
+
+
+    }
+
+}
