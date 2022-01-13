7b8f7ddaabccc2f3687dc0a91f76964de2711899
ys200209 <lys200209@naver.com>
2022-01-13 22:31:59 +0900

    Binary_Search Algorithm

--- 15/Main15_27.java
+++ 15/Main15_27.java
@@ -4,7 +4,8 @@ import java.util.StringTokenizer;
 public class Main15_27 {
     static int N, X, lower, upper;
     static int[] A;
-
+    static boolean exist;
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,48 +16,53 @@ public class Main15_27 {
         A = new int[N];
 
         st = new StringTokenizer(br.readLine(), " ");
-        int i=0;
+        int i = 0;
         while(st.hasMoreTokens()) {
             A[i] = Integer.parseInt(st.nextToken());
             i++;
         }
 
-        searchLower(X, 0, N);
+        lowerSearch(X, 0, N-1);
+        upperSearch(X, 0, N-1);
 
-        searchUpper(X, 0, N);
+        System.out.println("lower : " + lower + ", upper : " + upper);
 
-        System.out.println("lower = " + lower);
-        System.out.println("upper = " + upper);
+        if (exist) System.out.println(upper - lower + 1);
+        else System.out.println("-1");
+        
+
     }
 
-    public static void searchLower(int target, int front, int back) {
+    public static void lowerSearch(int target, int front, int back) {
         if (front > back) return;
 
-        int mid = (front+back) / 2;
+        int mid = (front + back) / 2;
 
         if (A[mid] > target) {
-            searchLower(target, front, mid-1);
+            lowerSearch(target, front, mid-1);
         } else if (A[mid] < target) {
-            searchLower(target, mid+1, back);
+            lowerSearch(target, mid+1, back);
         } else {
             lower = mid;
-            searchLower(target, front, mid-1);
+            exist = true;
+            lowerSearch(target, front, mid-1);
         }
     }
 
-    public static void searchUpper(int target, int front, int back) {
-        if (front >= back) return;
+    public static void upperSearch(int target, int front, int back) {
+        if (front > back) return;
 
-        int mid = (front+back) / 2;
+        int mid = (front + back) / 2;
 
         if (A[mid] > target) {
-            searchUpper(target, mid+1, back);
+            upperSearch(target, front, mid-1);
         } else if (A[mid] < target) {
-            searchUpper(target, front, mid-1);
+            upperSearch(target, mid+1, back);
         } else {
             upper = mid;
-            searchUpper(target, mid+1, back);
+            exist = true;
+            upperSearch(target, mid+1, back);
         }
     }
-    
+
 }
