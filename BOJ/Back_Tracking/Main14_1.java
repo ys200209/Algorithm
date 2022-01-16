88eda621c602168c680f8342acc577e2e3767ac8
ys200209 <lys200209@naver.com>
2022-01-16 20:14:10 +0900

    Back_Tracking Algorithm

---
+++ BOJ/Back_Tracking/Main14_1.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main14_1 {
+    static int N, M;
+    static int[] A;
+    static String str="";
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        A = new int[N];
+        visited = new boolean[N];
+
+        for(int i=0; i<N; i++) {
+            A[i] = i+1;
+        }
+
+        DFS(0);
+
+        System.out.println(sb);
+    }
+
+    public static void DFS(int count) {
+        if (count == M) {
+            sb.append(str + "\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if (!visited[i]) {
+                str += (A[i] + " ");
+                visited[i] = true;
+                count += 1;
+                DFS(count);
+                count -= 1;
+                visited[i] = false;
+                str = str.substring(0, str.length()-2);
+            }
+        }
+
+    }
+    
+}
