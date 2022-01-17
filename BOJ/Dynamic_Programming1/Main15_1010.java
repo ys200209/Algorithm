1449b85b820d7ab860d292c9cfdbcded7da91607
ys200209 <lys200209@naver.com>
2022-01-17 21:52:13 +0900

    Dynamic Programming Algorithm

---
+++ BOJ/Dynamic_Programming1/Main15_1010.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main15_1010 {
+    static int T, N, M;
+    static int[][] d = new int[31][31];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+
+        }
+
+
+    }
+    
+}
