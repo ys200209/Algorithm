791f4cd208f7f1a0e14474aa777d4899575567e5
ys200209 <lys200209@naver.com>
2022-01-21 15:21:53 +0900

    DFS/BFS Algorithm

--- BOJ/DFS_BFS/Main23_7.java
+++ BOJ/DFS_BFS/Main23_7.java
@@ -1,88 +1,28 @@
+import java.io.*;
 import java.util.*;
 
 class Main23_7 {
-    public static int N, K, result=0;
-    public static Queue<Integer> queue = new LinkedList<>();
-    public static boolean[] visited;
-
+    static int N, M, H;
+    static int[][] box;
+ 
     public static void main(String[] args) {
 
-        // 백준 온라인 저지 DFS/BFS(23)의 7번
-        
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = scanner.nextInt();
-        K = scanner.nextInt();
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[100001];
-
-        if (N != K) BFS();
-
-        System.out.println(result);
-        
-    }
-
-    public static void BFS() {
-
-        queue.offer(N);
-        visited[N] = true;
-
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            int moveTo;
-
-            for(int i=0; i<size; i++) {
-                int temp = queue.poll();
-                
-
-                if (N < K) { // N이 K보다 작다면 더하든 빼든 멀 하든 상관없다.
-                    moveTo = temp-1;
-                    if (moveTo >= 0) {
-                        if (!visited[moveTo]) { // 이미 탐색했던 곳은 더는 탐색하지 않도록.
-                            queue.offer(moveTo);
-                            visited[moveTo] = true;
-                        }
-                    }
-
-                    moveTo = temp+1;
-                    if (moveTo < visited.length) {
-                        if (!visited[moveTo]) { // 이미 탐색했던 곳은 더는 탐색하지 않도록.
-                            queue.offer(moveTo); 
-                            visited[moveTo] = true;
-                        }
-                    }
-
-                    moveTo = temp*2;
-                    if (moveTo < visited.length) {
-                        
-                        if (!visited[moveTo]) { // 이미 탐색했던 곳은 더는 탐색하지 않도록.
-                            queue.offer(moveTo);
-                            visited[moveTo] = true;
-                        }
-                    }
-                    
-                    
-                } else { // N이 K보다 크다면 빼야만 한다.
-                    moveTo = temp-1;
-                    if (moveTo >= 0) {
-                        if (!visited[moveTo]) { 
-                            queue.offer(moveTo);
-                            visited[moveTo] = true;
-                        }
-                    }
-                }
-
-                if (visited[K]) { 
-                    result += 1;
-                    return; // 동생과 위치가 같아졌다면 그만둔다.
-                }
-
-            }
-
-            result += 1;
-            
+        int j=0;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            box[i][j] = Integer.parseInt(st.nextToken());
+            j++;
         }
 
+        
+
     }
-    
+
 }
