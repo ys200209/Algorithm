162581d2469c5f0b2ca92077e40a0db7a281f614
ys200209 <lys200209@naver.com>
2022-01-23 19:35:09 +0900

    Shortest_Path Algorithm

--- BOJ/Shortest_Path/Main25_5.java
+++ BOJ/Shortest_Path/Main25_5.java
@@ -2,9 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main25_5 {
-    
-    public static void main(String[] args) {
-        
-    }
+    public static int INF = (int) 1e9;
+	public static int N, M;
+	public static int[][] graph;
+    public static StringBuilder sb = new StringBuilder();
 
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		graph = new int[N+1][N+1];
+		
+		for(int i=1; i<=N; i++) {
+			Arrays.fill(graph[i], INF);
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if (i == j) graph[i][j] = 0;
+			}
+		}
+		
+		for(int i=0; i<M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			graph[a][b] = Math.min(graph[a][b], c);
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+				}
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if (graph[i][j] == INF) sb.append("0 ");
+				else sb.append(graph[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
