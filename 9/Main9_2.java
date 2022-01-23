28bedc114a1e1ff179d8e20a76a3741eceb5b321
ys200209 <lys200209@naver.com>
2022-01-23 19:34:51 +0900

    Shortest_Path Algorithm

--- 9/Main9_2.java
+++ 9/Main9_2.java
@@ -1,43 +1,57 @@
+import java.io.*;
 import java.util.*;
 
 public class Main9_2 {
-    public static Queue<Nodei> queue = new LinkedList<>();
+    public static int INF = (int) 1e9;
+	public static int N, M, X, K;
+	public static int[][] graph;
 
-    public static void main(String[] args) {
-
-        queue.offer(new Nodei(1, 10));
-        queue.offer(new Nodei(2, 20));
-        queue.offer(new Nodei(2, 30));
-
-        queue.forEach((o) -> {
-            o.distance = o.getDistance()+1;
-        });
-
-        while(!queue.isEmpty()) {
-            Nodei node = queue.poll();
-            System.out.println(node.getName() + " : " + node.getDistance());
-        }
-
-    }
-    
-}
-
-class Nodei {
-
-    public int name;
-    public int distance;
-
-    public Nodei(int name, int distance) {
-        this.name = name;
-        this.distance = distance;
-    }
-
-    public int getName() {
-        return this.name;
-    }
-
-    public int getDistance() {
-        return this.distance;
-    }
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new int[N+1][N+1];
+		
+		for(int i=0; i<=N; i++) {
+			Arrays.fill(graph[i], INF);
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if (i == j) graph[i][j] = 0;
+			}
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a][b] = 1;
+			graph[b][a] = 1;
+		}
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		X = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+				}
+			}
+		}   
+		
+		int distance = graph[1][K] + graph[K][X];
+		
+        if (distance >= INF) System.out.println("-1");
+        else System.out.println(distance);
+	}
+	
+	
 }
