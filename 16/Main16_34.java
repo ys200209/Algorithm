59266cb8307c801395f647cd29a933718db84062
ys200209 <lys200209@naver.com>
2022-01-26 21:06:22 +0900

    Dynamic Programming Algorithm

---
+++ 16/Main16_34.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main16_34 {
+    static int N, result=1;
+    static int[] A, d;
+
+    public static void main(String[] args) throws IOException {
+
+        // 15:37
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        A = new int[N+1];
+        d = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int i=1;
+        while(st.hasMoreTokens()) {
+            A[i] = Integer.parseInt(st.nextToken());
+            d[i] = 1;
+            i++;
+        }
+
+        for(i=2; i<=N; i++) {
+            for(int j=i-1; j>=1; j--) {
+                if (A[i] < A[j]) {
+                    d[i] = Math.max(d[i], d[j]+1);
+                    result = Math.max(result, d[i]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(d));
+        
+        System.out.println(N - result);
+
+    }
+    
+}
