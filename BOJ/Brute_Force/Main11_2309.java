3fd4f2e64210a6e16539bfd06450ae7a319adaf3
ys200209 <lys200209@naver.com>
2022-01-28 21:44:08 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_2309.java
+++ BOJ/Brute_Force/Main11_2309.java
@@ -2,46 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main11_2309 {
-    static ArrayList<Integer> result = new ArrayList<>();
-    static int[] A = new int[9];
+    static int sum=0;
+    static int[] A;
     static StringBuilder sb = new StringBuilder();
-    static boolean isSearch = false;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
+        A = new int[9];
         for(int i=0; i<9; i++) {
             A[i] = Integer.parseInt(br.readLine());
+            sum += A[i];
         }
 
-        DFS(0, 0);
+        Arrays.sort(A);
+        sum -= 100;
 
-        System.out.println(sb);
-    }
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if (i == j) continue;
 
-    public static void DFS(int i, int count) {
-        if (count == 7) {
-            int sum = 0;
-            for(int j=0; j<7; j++) {
-                sum += result.get(j);
-            }
-
-            if (sum == 100) {
-                Collections.sort(result);
-                for(int j=0; j<7; j++) {
-                    sb.append(result.get(j) + "\n");
+                if (A[i] + A[j] == sum) {
+                    for(int k=0; k<9; k++) {
+                        if (k != i && k != j) {
+                            sb.append(A[k] + "\n");
+                        }
+                    }
+                    System.out.println(sb);
+                    return;
                 }
-                isSearch = true;
             }
-            return;
-        }
-
-        for(int j=i; j<9; j++) {
-            result.add(A[i]);
-            DFS(j+1, count+1);
-            if (isSearch) return;
-            result.remove(result.size()-1);
         }
     }   
 }
