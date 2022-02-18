ec6e6f1375760cec136f39e3732d37d1c63b3dbc
ys200209 <lys200209@naver.com>
2022-02-18 15:13:13 +0900

    BOJ Dynamic_Programming Algorithm

---
+++ BOJ/Dynamic_Programming-Shortest_Path/Main27_8.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main27_8 {
+
+    public static void main(String[] args) {
+
+        int number = 1;
+
+        System.out.println(number = number%1000 * 10 + number/1000);
+
+        System.out.println(number = number == 0 ? 9999 : number-1);
+
+        System.out.println(number = (number*2)%10000);
+
+        //System.out.println(number = (number*2)%10000);
+
+
+    }
+    
+}
