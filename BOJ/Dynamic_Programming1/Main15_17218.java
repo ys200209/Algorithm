eb48eb7472945f0f23d31367ab5f382687b3c7f4
ys200209 <lys200209@naver.com>
2022-02-20 14:56:49 +0900

    BOJ Dynamic_Programming Algorithm

---
+++ BOJ/Dynamic_Programming1/Main15_17218.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main15_17218 {
+    static String s1, s2;
+    static String[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        s1 = br.readLine();
+        s2 = br.readLine();
+        dp = new String[41][41];
+
+        for(int i=0; i<dp.length; i++) {
+            Arrays.fill(dp[i], "");
+        }
+
+        for(int i=1; i<=s1.length(); i++) {
+            for(int j=1; j<=s2.length(); j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + s1.substring(i-1, i);
+                } else {
+                    dp[i][j] = dp[i-1][j].length() > dp[i][j-1].length() ? dp[i-1][j] : dp[i][j-1];
+                }
+            }
+        }
+
+        System.out.println(dp[s1.length()][s2.length()]);
+
+    }
+
+
+}
