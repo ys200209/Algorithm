3e775dfb0109136c2c2562cb02b3841b6a6c86fe
ys200209 <lys200209@naver.com>
2022-02-21 00:54:57 +0900

    BOJ Tree Algorithm

--- BOJ/Tree/Main28_2.java
+++ BOJ/Tree/Main28_2.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main28_2 {
-    static int V, MAX=0, d=0, node;
+    static int V, n, MAX=0;
+    // static int[][] graph;
     static ArrayList<Node>[] graph;
-    static Queue<Integer> queue = new LinkedList<>();
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -13,6 +13,7 @@ public class Main28_2 {
 
         V = Integer.parseInt(br.readLine());
         graph = new ArrayList[V+1];
+        visited = new boolean[V+1];
 
         for(int i=0; i<=V; i++) {
             graph[i] = new ArrayList<>();
@@ -20,45 +21,48 @@ public class Main28_2 {
 
         for(int i=1; i<=V; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            st.nextToken();
-
-            while(true) {
+            
+            int start = Integer.parseInt(st.nextToken());
+            while(st.hasMoreTokens()) {
+                
                 int node = Integer.parseInt(st.nextToken());
                 if (node == -1) break;
-                int distance = Integer.parseInt(st.nextToken());
+                int dis = Integer.parseInt(st.nextToken());
 
-                graph[i].add(new Node(node, distance));
-                // 1 (2)-> 3 (3)-> 4 (4)-> 2 -> 3
+                graph[start].add(new Node(node, dis));
             }
         }
-        
-        visited = new boolean[V+1];
-        DFS(1, 0); // (1, dis);
-        System.out.println("(1) MAX : " + MAX);
 
-        visited = new boolean[V+1];
-        DFS(node, 0); // (node, dis);
-        System.out.println("(2) MAX : " + MAX);
-
+        /*for(int i=0; i<=V; i++) {
+            System.out.println("\ni : " + i);
+            for(int j=0; j<graph[i].size(); j++) {
+                System.out.print(graph[i].get(j).index + " ");
+            }
+        }*/
         
+        
+        DFS(1, 0);
+        visited = new boolean[V+1];
+
+        DFS(n, 0);
+        System.out.println(MAX);
     }
 
-    public static void DFS(int point, int dis) {
-        if (dis > MAX) {
-            MAX = dis;
-            node = point;
-        }
-
-        visited[point] = true;
-
-        for(Node n : graph[point]) {
-            if (!visited[n.getIndex()]) {
-                visited[n.getIndex()] = true;
-                DFS(n.getIndex(), n.getDistance() + dis);
-            }
+    public static void DFS(int v, int len) {
+        if (len > MAX) {
+            n = v;
+            MAX = len;
         }
         
+        visited[v] = true;
 
+        for(Node node : graph[v]) {
+            if (!visited[node.index]) {
+                
+                visited[node.index] = true;
+                DFS(node.index, len + node.distance);
+            }
+        }
     }
 
 }
@@ -71,14 +75,6 @@ class Node {
     public Node(int index, int distance) {
         this.index = index;
         this.distance = distance;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public int getDistance() {
-        return distance;
     }
 
 }
