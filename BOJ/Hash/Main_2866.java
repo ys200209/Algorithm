edee3bd8e5f449c84422e95843cdaaaed8fa00e3
ys200209 <lys200209@naver.com>
2022-02-23 23:24:23 +0900

    BOJ Hash Algorithm

---
+++ BOJ/Hash/Main_2866.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_2866 {
+    static int R, C, count=0;
+    static String[][] table;
+    static Map<Integer, String> map = new HashMap<>();
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        
+    }
+}
