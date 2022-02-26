080dcf9d4593f0caae953ea671b5cb8b1698f0c3
ys200209 <lys200209@naver.com>
2022-02-27 03:47:16 +0900

    BOJ Greedy Algorithm

---
+++ BOJ/Greedy/Main16_1213.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main16_1213 {
+    static String str;
+    
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        
+        
+       
+        
+        return;
+    }
+}
