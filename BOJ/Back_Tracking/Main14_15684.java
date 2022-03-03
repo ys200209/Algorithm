76ae339bf5c8e8f7fe7d0ec47e6c663c0dbb23de
ys200209 <lys200209@naver.com>
2022-03-04 00:29:07 +0900

    BOJ Back_Tracking Algorithm

---
+++ BOJ/Back_Tracking/Main14_15684.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main14_15684 {
+    
+    
+    public static void main(String[] args) throws IOException {
+
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println(Math.cbrt(8));
+
+    }
+
+}
