c0e44b0504b62a2828641187101608a5b7436823
ys200209 <lys200209@naver.com>
2022-03-06 23:07:58 +0900

    BOJ Greedy Algorithm

---
+++ BOJ/Greedy/Main16_6064.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main16_6064 {
+    static int T, N, M, X, Y;
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken());
+            Y = Integer.parseInt(st.nextToken());
+
+        }
+
+        
+
+    }
+
+}
