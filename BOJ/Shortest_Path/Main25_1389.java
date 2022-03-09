4186f6e41abde1d3dfdacc173dc8c9936a006c13
ys200209 <lys200209@naver.com>
2022-03-10 01:29:43 +0900

    BOJ Shortest_Path Algorithm

---
+++ BOJ/Shortest_Path/Main25_1389.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main25_1389 {
+    static int INF = (int)1e9;
+    static int N, M;
+    static int[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dp = new int[N+1][N+1];
+        for(int i=0; i<=N; i++) {
+            Arrays.fill(dp[i], INF);
+            for(int j=0; j<=N; j++) {
+                if (i == j) dp[i][j] = 0;
+            }
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            dp[A][B] = 1;
+            dp[B][A] = 1;
+        }
+
+        int index=0, distance = (int)1e9;
+
+        for(int k=1; k<=N; k++) {
+            for(int i=1; i<=N; i++) {
+                for(int j=1; j<=N; j++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+
+        for(int i=1; i<=N; i++) {
+            int dis = 0;
+            for(int j=1; j<=N; j++) dis += dp[i][j];
+            // System.out.println(i + " : " + dis);
+            if (distance > dis) {
+                index = i;
+                distance = dis;
+            }
+        }
+
+        /*System.out.println("-----------[dp]----------");
+        for(int i=0; i<=N; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }*/
+        System.out.println(index);
+
+    }
+
+}
