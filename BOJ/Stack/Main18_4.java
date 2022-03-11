dbac152e70b26824469bab5f09590eb20b8b8b51
ys200209 <lys200209@naver.com>
2022-03-12 01:05:23 +0900

    BOJ Stack Algorithm

--- BOJ/Stack/Main18_4.java
+++ BOJ/Stack/Main18_4.java
@@ -1,54 +1,50 @@
 import java.util.*;
+import java.io.*;
 
 public class Main18_4 {
-    public static String result = "YES";
-    public static String[] S;
-    public static Stack<String> stack = new Stack<>();
-    
-    public static void main(String[] args) {
+    static String S[];
+    static Stack<String> stack;
+    static StringBuilder sb = new StringBuilder();
 
-        // 백준 온라인 저지 스택(18)의 4번
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        stack.clear();
-        S = scanner.nextLine().split("");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true) {
+            S = br.readLine().split("");
+            stack = new Stack<>();
 
+            if (S[0].equals(".") && S.length == 1) break;
+
+            if (search()) sb.append("yes\n");
+            else sb.append("no\n");
+
+        }
+        
+        System.out.println(sb);
+        
+    }
+
+    public static boolean search() {
         for(int i=0; i<S.length; i++) {
-                if (S[i].equals(".")) {
-                    break;
-                }
-                if (S[i].equals("(") || S[i].equals("[")) {
-                    stack.push(S[i]);
-                }
-                if (S[i].equals(")")) {
-                    if (!stack.isEmpty()) {
-                        if (stack.peek().equals("(")) {
-                            stack.pop();
-                        } else {
-                            result = "NO";
-                            break;
-                        }
+            if (S[i].equals("(") || S[i].equals("[")) stack.push(S[i]);
+            else if (S[i].equals(")") || S[i].equals("]")) {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (S[i].equals(")") && !stack.peek().equals("(")) {
+                        return false;
+                    } else if (S[i].equals("]") && !stack.peek().equals("[")) {
+                        return false;
                     } else {
-                        result = "NO";
-                        break;
-                    }
-                } else if (S[i].equals("]")) {
-                    if (!stack.isEmpty()) {
-                        if (stack.peek().equals("[")) {
-                            stack.pop();
-                        } else {
-                            result = "NO";
-                            break;
-                        }
-                    } else {
-                        result = "NO";
-                        break;
+                        stack.pop();
                     }
                 }
             }
-    
-            System.out.println(result);
-        
+        }
+
+        if (stack.isEmpty()) return true;
+        else return false;
     }
 
 }
