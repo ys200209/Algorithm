76b36519b6e74d1e805e89eef727e1bafa8d7dc6
ys200209 <lys200209@naver.com>
2022-03-12 21:48:19 +0900

    BOJ DFS_BFS Algorithm

--- BOJ/DFS_BFS/Main23_13460.java
+++ BOJ/DFS_BFS/Main23_13460.java
@@ -2,10 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main23_13460 {
+    static RED redBall;
+    static BLUE blueBall;
+    static int[] PASS = new int[2];
     static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
     static int[] dy = {0, 0, -1, 1};
     static int N, M, result=0;
     static String[][] board;
+    static Queue<Point> queue = new LinkedList<>();
     
     public static void main(String[] args) throws IOException {
 
@@ -15,124 +19,157 @@ public class Main23_13460 {
         M = Integer.parseInt(st.nextToken());
         board = new String[N][M];
 
-        Point RED = null, BLUE = null;
         for(int i=0; i<N; i++) {
             board[i] = br.readLine().split("");
             for(int j=0; j<M; j++) {
-                if (board[i][j].equals("R")) RED = new Point("R", i, j);
-                if (board[i][j].equals("B")) BLUE = new Point("B", i, j);
+                if (board[i][j].equals("R")) redBall = new RED("R", i, j, new boolean[N][M], false);
+
+                if (board[i][j].equals("B")) blueBall = new BLUE("B", i, j, new boolean[N][M], false);
+
+                if (board[i][j].equals("O")) {
+                    PASS[0] = i;
+                    PASS[1] = j;
+                }
             }
         }
 
-        DFS(board, 0, RED, BLUE);
+        queue.offer(new Point(board, redBall, blueBall, 0));
+        result = BFS();
 
+        System.out.println("result : " + result);
+        
     }
 
-    public static void DFS(String[][] b, int count, Point RED, Point BLUE) {
-        if (count > 10) {
-            result = -1;
-            return;
-        }
+    public static int BFS() {
 
-        int rx = RED.x;
-        int ry = RED.y;
-        int bx = BLUE.x;
-        int by = BLUE.y;
+        while(!queue.isEmpty()) {
+            Point point = queue.poll();
+            int rx = point.redBall.x;
+            int ry = point.redBall.y;
+            int bx = point.blueBall.x;
+            int by = point.blueBall.y;
 
-        for(int i=0; i<4; i++) {
-            String[][] tempBoard = setBoard(b);
-            Point tempRED = new Point("R", RED.x, RED.y);
-            Point tempBLUE = new Point("B", BLUE.x, BLUE.y);
+            if (point.count > 10) continue;
 
-            if (i == 0 || i == 1) { // [상, 하]
-                if (rx == bx) { // 같은 x좌표 인가?
-                    if (i == 0) // [상] 이라면
-                        if (ry < by) MOVE(tempBoard, tempRED, i); // 위쪽에 있는 구슬 먼저 위로 움직여준다.
-                        else MOVE(tempBoard, tempBLUE, i);
-                    else // [하] 라면
-                        if (ry > by) MOVE(tempBoard, tempRED, i); // 아랫쪽에 있는 구슬 먼저 아래로 움직여준다.
-                        else MOVE(tempBoard, tempBLUE, i);
-                } else { // 같은 x좌표가 아니라면 누굴 먼저 움직이든 상관없음
-                    MOVE(tempBoard, tempRED, i);
-                    MOVE(tempBoard, tempBLUE, i);
-                }
-            } else { // [좌, 우]
-                if (ry == by) { // 같은 y좌표 인가?
-                    if (i == 2) { // [좌] 라면
-                        if (rx < bx) MOVE(tempBoard, tempRED, i); // 왼쪽에 있는 구슬 먼저 왼쪽으로 움직여준다.
-                        else MOVE(tempBoard, tempBLUE, i);
-                    } else { // [우] 라면
-                        if (rx > bx) MOVE(tempBoard, tempRED, i); // 오른쪽에 있는 구슬 먼저 오른쪽으로 움직여준다.
-                        else MOVE(tempBoard, tempBLUE, i);
+            for(int i=0; i<4; i++) {
+                int nx = rx + dx[i];
+                int ny = ry + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+                if (point.redBall.visited[nx][ny]) continue;
+                
+                point = MOVE(point, "R", i);
+
+                if (point != null) {
+                    if (point.redBall.visited[PASS[0]][PASS[1]]) {
+                        if (point.blueBall.visited[PASS[0]][PASS[1]]) return -1;
+                        else return point.count;
                     }
-                } else {
-                    // 같은 y좌표가 아니라면 누굴 먼저 움직이든 상관없음
-                    MOVE(tempBoard, tempRED, i);
-                    MOVE(tempBoard, tempBLUE, i);
+
+                    queue.offer(new Point(point.board, point.redBall, point.blueBall, point.count+1));
                 }
-            }
-
-            if ((tempRED == null && tempBLUE == null) || (tempRED != null && tempBLUE == null)) {
-                System.out.println("-1");
-                System.exit(0);
-            } else if (tempRED == null && tempBLUE != null) {
-                System.out.println(count);
-                System.exit(0);
-            }
-
-            DFS(tempBoard, count+1, tempRED, tempBLUE);
-
+            }   
         }
 
-
+        return -1;
     }
 
-    public static void MOVE(String[][] temp, Point Who, int vector) {
-        int nx = Who.x + dx[vector];
-        int ny = Who.y + dy[vector];
-        if (temp[nx][ny].equals("O")) {
-            Who = null;
-            return;
-        }
+    public static Point MOVE(Point point, String color, int vector) {
 
-        while(temp[nx][ny].equals(".")) {
-            nx += dx[vector];
-            ny += dy[vector];
-            if (temp[nx][ny].equals("O")) {
-                Who = null;
-                return;
+        if (color.equals("R")) {
+            int nx = point.redBall.x + dx[vector];
+            int ny = point.redBall.y + dy[vector];
+
+            int blueCount = 0;
+            while(true) {
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) return point;
+
+                if (point.board[nx][ny].equals("#")) return point;
+
+                if (point.redBall.visited[nx][ny]) return null;
+
+                if (point.board[nx][ny].equals("B")) {
+                    if (blueCount == 1) return point;
+                    MOVE(point, "B", vector);
+                    blueCount++;
+                    continue;
+                }
+
+                point.redBall.visited[nx][ny] = true;
+                point.board[nx - dx[vector]][ny - dy[vector]] = ".";
+                point.board[nx][ny] = "R";
+                nx += dx[vector];
+                ny += dy[vector];
+            }
+
+        } else {
+            int nx = point.blueBall.x + dx[vector];
+            int ny = point.blueBall.y + dy[vector];
+
+            while(true) {
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) return point;
+
+                if (point.board[nx][ny].equals("#")) return point;
+
+                point.blueBall.visited[nx][ny] = true;
+                point.board[nx - dx[vector]][ny - dy[vector]] = ".";
+                point.board[nx][ny] = "B";
+                nx += dx[vector];
+                ny += dx[vector];
             }
         }
-
-        temp[Who.x][Who.y] = ".";
-        Who.x = nx - dx[vector]; // while문에 의해서 "."이 아닌 좌표까지 움직였기 때문에 
-        Who.y = ny - dy[vector]; // 값을 저장할 때는 마지막 "."이 아닌 한 좌표값을 빼준만큼 저장함.
-        temp[Who.x][Who.y] = Who.color;
-    }
-
-    public static String[][] setBoard(String[][] b) {
-        String[][] tempBoard = new String[b.length][b[0].length];
-
-        for(int i=0; i<b.length; i++) {
-            for(int j=0; j<b[0].length; j++) {
-                tempBoard[i][j] = b[i][j];
-            }
-        }
-        return tempBoard;
     }
 
 }
 
 class Point {
 
+    String[][] board;
+    RED redBall;
+    BLUE blueBall;
+    int count;
+
+    public Point(String[][] board, RED redBall, BLUE blueBall, int count) {
+        this.board = board;
+        this.redBall = redBall;
+        this.blueBall = blueBall;
+        this.count = count;
+    }
+
+}
+
+class RED {
+
     String color;
     int x;
     int y;
+    boolean[][] visited; 
+    boolean isPass;
 
-    public Point(String color, int x, int y) {
+    public RED(String color, int x, int y, boolean[][] visited, boolean isPass) {
         this.color = color;
         this.x = x;
         this.y = y;
+        this.visited = visited;
+        this.isPass = isPass;
+    }
+}
+
+class BLUE {
+
+    String color;
+    int x;
+    int y;
+    boolean[][] visited;
+    boolean isPass;
+
+    public BLUE(String color, int x, int y, boolean[][] visited, boolean isPass) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.visited = visited;
+        this.isPass = isPass;
     }
 
 }
