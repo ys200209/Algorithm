5d343011a3f278f412364fab413a44756467922e
ys200209 <lys200209@naver.com>
2022-03-17 21:40:50 +0900

    BOJ Dynamic Programming

--- BOJ/Dynamic_Programming1/Main15_2293.java
+++ BOJ/Dynamic_Programming1/Main15_2293.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main15_2293 {
-    static int N, K, result=0;
+    static int N, K;
     static int[] coin, dp;
 
     public static void main(String[] args) throws IOException {
@@ -12,9 +12,20 @@ public class Main15_2293 {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        coin = new int[N+1];
+        coin = new int[N];
         dp = new int[K+1];
+
+        for(int i=0; i<N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[0] = 1;
+        for(int i=0; i<N; i++) {
+            for(int j=coin[i]; j<=K; j++) {
+                dp[j] = dp[j] + dp[j-coin[i]];
+            }
+            System.out.println(Arrays.toString(dp));
+        }
 
         
         
