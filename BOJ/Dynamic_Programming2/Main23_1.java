93d33e411cb3adf71ba5e38bffad5ccb4a08ddc8
ys200209 <lys200209@naver.com>
2022-03-31 00:11:04 +0900

    BOJ Dynamic_Programming

--- BOJ/Dynamic_Programming2/Main23_1.java
+++ BOJ/Dynamic_Programming2/Main23_1.java
@@ -6,13 +6,9 @@ public class Main23_1 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int X = Integer.parseInt(br.readLine());
-        int Y = Integer.parseInt(br.readLine());
-        
-        if (X > 0 && Y > 0) System.out.println(1);
-        else if (X < 0 && Y > 0) System.out.println(2);
-        else if (X < 0 && Y < 0) System.out.println(3);
-        else System.out.println(4);
+        String str = br.readLine();
+
+        System.out.println(str + "??!");
     }
 
 }
