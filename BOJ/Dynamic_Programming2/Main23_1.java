21e2bd447600ba472f310f706340341c7f13a43c
ys200209 <lys200209@naver.com>
2022-04-02 01:18:44 +0900

    BOJ Dynamic_Programming

--- BOJ/Dynamic_Programming2/Main23_1.java
+++ BOJ/Dynamic_Programming2/Main23_1.java
@@ -2,20 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main23_1 {
+    static int result=0;
+    static int[] A;
     
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        A = new int[3];
 
-        int HH = Integer.parseInt(st.nextToken());
-        int MM = Integer.parseInt(st.nextToken());
+        int i=0;
+        while(st.hasMoreTokens()) {
+            A[i] = Integer.parseInt(st.nextToken());
+            i++;
+        }
 
-        int late = Integer.parseInt(br.readLine());
+        Arrays.sort(A);
 
-        HH += ((MM + late) / 60);
-        MM = (MM + late) % 60;
-
-        System.out.println((HH%24) + " " + MM);
+        if (A[0] == A[1] && A[1] == A[2]) {
+            result = 10000 + A[0]*1000;
+        } else if (A[0] == A[1] || A[1] == A[2]) {
+            result = 1000 + A[1]*100;
+        } else {
+            result = A[2]*100;
+        }
+        
+        System.out.println(result);
     }
 }
