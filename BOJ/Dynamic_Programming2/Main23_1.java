0bfb680990e9ca7c5b27d559386b792511216f84
ys200209 <lys200209@naver.com>
2022-04-01 20:52:44 +0900

    BOJ Dynamic_Programming

--- BOJ/Dynamic_Programming2/Main23_1.java
+++ BOJ/Dynamic_Programming2/Main23_1.java
@@ -6,9 +6,16 @@ public class Main23_1 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        System.out.println(str + "??!");
+        int HH = Integer.parseInt(st.nextToken());
+        int MM = Integer.parseInt(st.nextToken());
+
+        int late = Integer.parseInt(br.readLine());
+
+        HH += ((MM + late) / 60);
+        MM = (MM + late) % 60;
+
+        System.out.println((HH%24) + " " + MM);
     }
-
 }
