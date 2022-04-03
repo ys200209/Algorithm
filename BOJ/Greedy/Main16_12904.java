4d364c8405a2a8f689ae0f85f5f35e4f0a14c220
ys200209 <lys200209@naver.com>
2022-04-04 00:17:02 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_12904.java
+++ BOJ/Greedy/Main16_12904.java
@@ -6,18 +6,14 @@ public class Main16_12904 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
         int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
 
-            sb.append((A+B) + "\n");
+        int sum = 0;
+
+        for(int i=1; i<=N; i++) {
+            sum += i;
         }
-        System.out.println(sb);
+
+        System.out.println(sum);
     }
 }
