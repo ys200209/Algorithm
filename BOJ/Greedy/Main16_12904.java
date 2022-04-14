ddfb629d711f51485067f1cd378eca275b6c1a3e
ys200209 <lys200209@naver.com>
2022-04-14 15:19:55 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_12904.java
+++ BOJ/Greedy/Main16_12904.java
@@ -2,28 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main16_12904 {
-    // static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int result = N;
-        int count=0;
+        int MIN=(int)1e9, MAX=0;
 
-        while(true) {
-            count++;
-
-            if (result < 10) {
-                result = result * 10 + result;
-            } else {
-                int A = result % 10;
-                int B = ((result/10) + (result%10)) % 10;
-                result = A * 10 + B;
-            }
-
-            if (result == N) break;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            MIN = Math.min(MIN, num);
+            MAX = Math.max(MAX, num);
         }
-        System.out.println(count);
+
+        System.out.println(MIN + " " + MAX);
     }
 }
