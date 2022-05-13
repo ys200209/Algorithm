5978e03f7fc017b856fbc3fc2af57d00a8dfe063
ys200209 <lys200209@naver.com>
2022-05-13 21:26:40 +0900

    2019 Kakao Blind

--- KAKAO INTERNSHIP/2019/Main2019_Internship_2.java
+++ KAKAO INTERNSHIP/2019/Main2019_Internship_2.java
@@ -4,31 +4,42 @@ class Main2019_Internship_2 {
 
     public static void main(String[] args) {
 
-        // System.out.println(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
+        System.out.println(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
         // [2, 1, 3, 4]
 
-        System.out.println(Arrays.toString(solution("{{20,111},{111}}")));
+        //System.out.println(Arrays.toString(solution("{{20,111},{111}}")));
         // 
 
     }
     
     public static int[] solution(String s) {
-        System.out.println("S : " + s);
-        s = s.replace("{", "");
-        System.out.println("S : " + s);
-
-        String[] A = s.split("\\},");
-        int[] answer = new int[A.length-1];
-        System.out.println(Arrays.toString(A));
         
-        Arrays.sort(A, (s1, s2) -> {
+        ArrayList<Integer> answer = new ArrayList<>();
+        s = s.substring(2,s.length());
+        s = s.substring(0,s.length()-2).replace("},{","-");
+        String str[] = s.split("-");
+
+        Arrays.sort(str, (s1, s2) -> {
             return s1.length() - s2.length();
         });
-
-        Set<Integer> set = new HashSet<>();
         
+        for(String numbers : str) {
+            String[] number = numbers.split(",");
 
-        return answer;
+            for(String n : number) {
+                if (!answer.contains(Integer.parseInt(n))) {
+                    answer.add(Integer.parseInt(n));
+                    break;
+                }
+            }
+        }
+        
+        int[] result = new int[answer.size()];
+
+        for(int i=0; i<answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+
+        return result;
     }
-
 }
