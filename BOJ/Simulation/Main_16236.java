f81aa6b8e12beb7a5debb4ec13a8e9bb601075b8
ys200209 <lys200209@naver.com>
2022-07-12 17:22:37 +0900

    BOJ Simulation Algorithm

--- BOJ/Simulation/Main_16236.java
+++ BOJ/Simulation/Main_16236.java
@@ -2,130 +2,124 @@ import java.util.*;
 import java.io.*;
 
 public class Main_16236 {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static int N, result = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N, M, result=0;
     static int[][] board;
-    static Shark shark;
+    static Queue<Shark> queue = new PriorityQueue<>();
     static ArrayList<Fish> fishes = new ArrayList<>();
-    static Queue<Shark> pq;
     static boolean[][] visited;
+    static Shark s;
     
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
         board = new int[N][N];
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int j=0;
-
             while(st.hasMoreTokens()) {
                 board[i][j] = Integer.parseInt(st.nextToken());
 
                 if (board[i][j] == 9) {
-                    shark = new Shark(i, j, 2, 0, 0);
+                    s = new Shark(i, j, 2, 0, 0);
                     board[i][j] = 0;
-                } else if (board[i][j] != 0) {
-                    fishes.add(new Fish(i, j, board[i][j], (int)1e9));
-                }
+                } else if (board[i][j] != 0) fishes.add(new Fish(i, j, board[i][j]));
 
                 j++;
             }
         }
 
-        Collections.sort(fishes, (f1, f2) -> {
-            return f1.size - f2.size;
+        Collections.sort(fishes, (fish1, fish2) -> {
+            return fish1.size - fish2.size;
         });
 
-        search();
-        
+        while(true) {
+            Fish fish = isEat();
+            if (fish == null) break;
+
+            s.x = fish.x;
+            s.y = fish.y;
+            s.eatCount++;
+
+            if (s.eatCount == s.size) {
+                s.eatCount = 0;
+                s.size++;
+            }
+
+            board[fish.x][fish.y] = 0;
+            fishes.remove(fish);
+        }
 
         System.out.println(result);
 
     }
 
-    public static void search() {
-        
-        while(true) {
-            Fish fish = null;
+    public static Fish isEat() {
+        Fish fish = null;
+        int distance = (int)1e9;
 
-            for(int i=0; i<fishes.size(); i++) {
-                if (fishes.get(i).size < shark.size) {
-                    visited = new boolean[N][N];
-                    pq = new PriorityQueue<>();
+        for (Fish f : fishes) {
+            if (s.size > f.size) {
+                int dis = BFS(f);
 
-                    BFS(fishes.get(i));
-
-                    if (fish == null) fish = fishes.get(i);
-                    else {
-                        if (fish.distance >= fishes.get(i).distance) {
-                            if (fish.distance == fishes.get(i).distance) {
-                                if (fish.x >= fishes.get(i).x) {
-                                    if (fish.x == fishes.get(i).x) {
-                                        if (fish.y > fishes.get(i).y) fish = fishes.get(i);
-                                    } else fish = fishes.get(i);
-                                }
-                            } else fish = fishes.get(i);
+                if (fish == null) {
+                    if (dis != (int)1e9) {
+                        distance = dis;
+                        fish = f;
+                    }
+                } else {
+                    if (distance > dis) {
+                        distance = dis;
+                        fish = f;
+                    } else if (distance == dis) { // 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기를 먹는다.
+                        if (fish.x > f.x) fish = f;
+                        else if (fish.x == f.x) { // 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
+                            if (fish.y > f.y) fish = f;
                         }
                     }
-                } else break;
+                }
+                
             }
-
-            if (fish != null) eat(fish);
-            else return;
         }
 
+        result += (distance == (int)1e9 ? 0 : distance);
+        return fish;
     }
 
-    public static void BFS(Fish fish) {
-        pq.offer(shark);
-        visited[shark.x][shark.y] = true;
+    public static int BFS(Fish fish) {
+        queue.clear();
+        visited = new boolean[N][N];
+        s.time = 0;
 
-        while(!pq.isEmpty()) {
-            Shark shark = pq.poll();
+        queue.offer(s);
+        visited[s.x][s.y] = true;
+
+        while(!queue.isEmpty()) {
+            Shark shark = queue.poll();
             int x = shark.x;
             int y = shark.y;
-            
+
             for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue; // 맵을 벗어난 영역이라면 중단
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-                if (visited[nx][ny]) continue; // 이미 지나갔던 곳이라면 중단
+                if (visited[nx][ny]) continue; // 이미 지나갔던 자리라면 지나갈 수 없음.
+                if (board[nx][ny] > shark.size) continue; // 자신보다 크기가 큰 물고기는 지나갈 수 없음.
 
-                if (board[nx][ny] > shark.size) continue; // 더 큰 물고기가 가로막고 있다면 중단
-
-                if (nx == fish.x && ny == fish.y) { // 물고기를 만났다면 중지
-                    fish.distance = shark.distance + 1;
-                    return;
-                }
+                if (fish.x == nx && fish.y == ny) return shark.time+1; // 물고기를 최단 시간에 발견했다면 종료
 
                 visited[nx][ny] = true;
-                pq.offer(new Shark(nx, ny, shark.size, shark.eat, shark.distance+1));
+                queue.offer(new Shark(nx, ny, shark.size, shark.eatCount, shark.time+1));
 
             }
         }
-    }
 
-    public static void eat(Fish fish) {
-        shark.x = fish.x;
-        shark.y = fish.y;
-        shark.eat++;
-        if (shark.eat == shark.size) {
-            shark.eat = 0;   
-            shark.size++;
-        }
-        shark.distance = 0;
-
-        result += fish.distance;
-        board[fish.x][fish.y] = 0;
-
-        fishes.remove(fish);
+        return (int)1e9;
     }
 
 }
@@ -135,20 +129,20 @@ class Shark implements Comparable<Shark> {
     int x;
     int y;
     int size;
-    int eat;
-    int distance;
+    int eatCount;
+    int time;
 
-    public Shark(int x, int y, int size, int eat, int distance) {
+    public Shark(int x, int y, int size, int eatCount, int time) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.eat = eat;
-        this.distance = distance;
+        this.eatCount = eatCount;
+        this.time = time;
     }
 
     @Override
-    public int compareTo(Shark shark) {
-        return this.distance - shark.distance;
+    public int compareTo(Shark s) {
+        return this.time - s.time;
     }
 
 }
@@ -158,13 +152,10 @@ class Fish {
     int x;
     int y;
     int size;
-    int distance;
 
-    public Fish(int x, int y, int size, int distance) {
+    public Fish(int x, int y, int size) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.distance = distance;
     }
-    
 }
