0cbcb36419b4a721202a7fd72d086f4ac1ff3caa
ys200209 <lys200209@naver.com>
2022-07-25 23:29:31 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_12904.java
+++ BOJ/Greedy/Main16_12904.java
@@ -2,20 +2,60 @@ import java.util.*;
 import java.io.*;
 
 public class Main16_12904 {
+    static String S, T;
+    static Deque<String> deque = new ArrayDeque<>();
+    static boolean isA = true;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int MIN=(int)1e9, MAX=-(int)1e9;
+        
+        S = br.readLine();
+        T = br.readLine();
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        while(st.hasMoreTokens()) {
-            int num = Integer.parseInt(st.nextToken());
-            MIN = Math.min(MIN, num);
-            MAX = Math.max(MAX, num);
+        int start = 0;
+        int index = 0;
+        while(!(S.length() == T.length())) {
+        // for(int i=0; i<6; i++) {
+            index = T.substring(start, T.length()).indexOf(S);
+            // System.out.println(start + " ~ " + T.length() + ", '" + S + "' = " + "index : " + index);
+
+            if (index == -1) {
+                if (isA) S = "B" + S;
+                else S += "B";
+
+                isA = !isA;
+                start = 0;
+                continue;
+            }
+
+            if (isA) {
+                if (T.length() <= start + index + S.length()) {
+                    isA = !isA;
+                    start = 0;
+                    S = "B" + S;
+                    continue;
+                } 
+
+                if (T.charAt(start + index + S.length()) == 'A') S += "A";
+                else start = (start+index+1);
+            } else {
+                if (index-1 < 0) {
+                    isA = !isA;
+                    start = 0;  
+                    continue;
+                }
+
+                if (T.charAt(index-1) == 'A') S = "A" + S;
+                else {
+                    S += "B";
+                    isA = !isA;
+                    start = 0;
+                }
+            }
         }
+        
+        System.out.println(S.equals(T) ? 1 : 0);
 
-        System.out.println(MIN + " " + MAX);
     }
 }
