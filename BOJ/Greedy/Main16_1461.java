a442119bdae0833908f6bd6ac7d0d0701ed108c5
ys200209 <lys200209@naver.com>
2022-08-06 17:45:07 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_1461.java
+++ BOJ/Greedy/Main16_1461.java
@@ -2,30 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main16_1461 {
-    static int N, M, result1=(int)1e9, result2=(int)1e9;
-    static Queue<Integer> pq1 = new PriorityQueue<>();
-    static Queue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
-    
-    public static void main(String[] args) throws IOException {
+    static int N, M;
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        while(st.hasMoreTokens()) {
-            int n = Integer.parseInt(st.nextToken());
-            pq1.offer(n);
-            pq2.offer(n);
-        }
-
         
 
-
-
-
     }
+    
 
 }
