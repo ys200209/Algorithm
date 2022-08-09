f8aa7d8ddf4106c12c586529467c3e149d6ab789
ys200209 <lys200209@naver.com>
2022-08-09 13:17:30 +0900

    BOJ Greedy Algorithm

--- BOJ/Greedy/Main16_11497.java
+++ BOJ/Greedy/Main16_11497.java
@@ -2,34 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main16_11497 {
-    static int T, N, result=0;
-    static int[] A, B;
+    static int T, N;
+    static int[] list;
+    static StringBuilder sb = new StringBuilder();
+    static Queue<Integer> pq;
     
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         T = Integer.parseInt(br.readLine());
         for(int t=0; t<T; t++) {
-            N = Integer.parseInt(br.readLine());
-            A = new int[N];
-            B = new int[N];
+            pq = new PriorityQueue<>();
 
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int i=0;
+            N = Integer.parseInt(br.readLine());
+            list = new int[N];
+            st = new StringTokenizer(br.readLine(), " ");
             while(st.hasMoreTokens()) {
-                A[i] = Integer.parseInt(st.nextToken());
-                i++;
+                pq.offer(Integer.parseInt(st.nextToken()));
+            }
+
+            int index=0;
+            while(!pq.isEmpty()) {
+                list[index] = pq.poll();
+                if (!pq.isEmpty()) list[N-index-1] = pq.poll();
+                index++;
             }
             
-            DFS(0);
-
+            int max = 0;
+            for(int i=0; i<N; i++) {
+                if (i == N-1) max = Math.max(max, Math.abs(list[N-1] - list[0]));
+                else max = Math.max(max, Math.abs(list[i] - list[i+1]));
+            }
+            sb.append(max + "\n");
         }
-
-    }
-
-    public static void DFS(int count) {
-
-        
+        System.out.println(sb);
     }
 
 }
