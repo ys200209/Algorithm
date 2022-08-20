3616c22d9563dd4778e88daa3451a301bdb8bf90
ys200209 <lys200209@naver.com>
2022-08-20 19:11:02 +0900

    Algorithm Test

--- ProgrammersTest/test3/ProgrammersTest3_2.java
+++ ProgrammersTest/test3/ProgrammersTest3_2.java
@@ -1,30 +1,37 @@
 import java.util.*;
 
 class ProgrammersTest3_2 {
-    static String search = "1231";
-    static Map<String, Integer> map = new HashMap<>();
+    static String bingo = "1231";
 
     public static void main(String[] args) {
 
         System.out.println(solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1})); // 2
+        // System.out.println(solution(new int[]{1,2,3,1,2,3,1,2,3,1})); // 1
 
     }
     
     public static int solution(int[] ingredient) {
         int answer = 0;
-
         StringBuilder sb = new StringBuilder();
-        for(int n : ingredient) {
+
+        for (int n : ingredient) {
             sb.append(n);
         }
 
-        String str = sb.toString();
-        while(str.contains(search)) {
-            str = str.replaceFirst(search, "");
-            answer++;
+        while(sb.indexOf(bingo) != -1) {
+            // int before = str.length();
+
+            System.out.println(sb);
+            
+            int index = sb.indexOf(bingo);
+            // int after = str.length();
+            sb = sb.replace(index, index+4, "");
+
+            // int result = before - after;
+            answer ++;
         }
 
         return answer;
     }
-
+    
 }
