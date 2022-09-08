fbba491d2db733a31baf18efd4a3dd9ef8a9fb30
ys200209 <lys200209@naver.com>
2022-09-08 20:18:50 +0900

    BOJ Implement Algorithm

---
+++ BOJ/Back_Tracking/Main14_.java
@@ -0,0 +1,15 @@
+package BOJ.Back_Tracking;
+
+import java.io.*;
+import java.util.*;
+
+public class Main14_ {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    }
+
+}
