2291a7e7f5c20574438ccb8e5f59ff25cafe8139
ys200209 <lys200209@naver.com>
2022-09-13 22:28:09 +0900

    BOJ BruteForce Algorithm

--- BOJ/Brute_Force/Main11_1436.java
+++ BOJ/Brute_Force/Main11_1436.java
@@ -1,33 +1,73 @@
-import java.util.*;
+package BOJ.Brute_Force;
+
 import java.io.*;
+import java.util.*;
 
 public class Main11_1436 {
-    
-    
+    static int N, count=0;
+
     public static void main(String[] args) throws IOException {
 
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        
-        Map<String, Integer> map = new HashMap<>();
-        map.put("AZZ", 5);
-        map.put("BBA", 3);
-        map.put("CAZ", 1);
+        /*front();
 
-        for(String s : map.keySet()) {
-            // System.out.println(s + " : " + map.get(s));
+        System.out.println("frontIndex : " + frontIndex + ", backIndex : " + backIndex);
+        System.out.println("frontNumber : " + frontNumber + ", backNumber : " + backNumber);*/
+
+        long index = 666;
+        while(true) {
+            while(!String.valueOf(index).contains("666")) {
+                index++;
+            }
+            count++;
+            if (count == N) {
+                System.out.println(index);
+                return;
+            }
+            index++;
         }
-
-        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, (entry1, entry2) -> {
-            return entry1.getValue() - entry2.getValue();
-        });
-
-        for(Map.Entry<String, Integer> entry : list) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-
     }
+
+    /*public static void front() {
+        if (count >= N) return;
+
+        System.out.println("FRONT");
+        long nextBackNumber = Long.parseLong(D + (backIndex+1));
+
+        while(nextBackNumber > Long.parseLong((frontIndex+1) + D)) {
+            if (count >= N) {
+                frontNumber = Long.parseLong(frontIndex + D);
+                return;
+            }
+
+            frontIndex++;
+            count++;
+        }
+
+        frontNumber = Long.parseLong(frontIndex + D);
+        back();
+    }
+
+    public static void back() {
+        if (count >= N) return;
+
+        System.out.println("BACK");
+        long nextFrontNumber = Long.parseLong((frontIndex+1) + D);
+
+        while(nextFrontNumber >= Long.parseLong(D + (backIndex+1))) {
+            if (count >= N) {
+                backNumber = Long.parseLong(D + backIndex);
+                return;
+            }
+
+            backIndex++;
+            count++;
+        }
+
+        backNumber = Long.parseLong(D + backIndex);
+        front();
+    }*/
 
 }
