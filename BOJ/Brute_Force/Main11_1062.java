dd9449c50cdca10cd53afd7031ae7066bccc98e3
ys200209 <lys200209@naver.com>
2022-09-16 22:27:32 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1062.java
+++ BOJ/Brute_Force/Main11_1062.java
@@ -1,43 +1,39 @@
 package BOJ.Brute_Force;
 
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main11_1062 {
+    static Stack<String> stack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
-//        System.out.println(Arrays.toString(solution(10, 2))); // "3people Unfollowed Me"
-//        System.out.println(Arrays.toString(solution(80, 1))); // "3people Unfollowed Me"
-        System.out.println(Arrays.toString(solution(18, 6))); // "3people Unfollowed Me"
+//        System.out.println(solution("baabaa")); // 1
+        System.out.println(solution("cdcd")); // 1
 //        System.out.println(solution(new int[][]{{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}})); // 120
 
     }
 
-    public static int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+    public static int solution(String s) {
+        String[] split = s.split("");
 
-        int sum = brown+yellow;
-        int width = 3;
-        int diff = (int)1e9;
-        while(true) {
-            if (width > sum/2) return answer;
-
-            if (sum % width == 0) {
-                int h = sum / width;
-
-                if (Math.abs(width - h) < diff && (width-2)*(h-2) == yellow) {
-                    diff = Math.abs(width - h);
-                    if (h > width) {
-                        answer[0] = h;
-                        answer[1] = width;
-                    } else {
-                        answer[0] = width;
-                        answer[1] = h;
-                    }
-                }
+        for(int i=1; i< split.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(split[i]);
+            } else {
+                if (stack.peek().equals(split[i])) stack.pop();
+                else stack.push(split[i]);
             }
-            width++;
         }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+    private static boolean isRemove() {
+        boolean isRemove = false;
+
+
+
+        return isRemove;
     }
 
 }
