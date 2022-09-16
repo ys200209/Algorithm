a4d4e95ad4351386ae94ed7326031292fddda48f
ys200209 <lys200209@naver.com>
2022-09-16 14:18:33 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1062.java
+++ BOJ/Brute_Force/Main11_1062.java
@@ -1,18 +1,53 @@
 package BOJ.Brute_Force;
 
-import java.util.*;
 import java.io.*;
 
 public class Main11_1062 {
-    static int N, K;
 
     
     public static void main(String[] args) throws IOException {
+        System.out.println(solution("  try    try   world  hello   try")); // "TrY HeLlO WoRlD"
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    }
 
+    public static String solution(String s) {
+        String answer = "";
+        String[] words = s.split(" ");
+        int index = 0;
+        for (String word : words) {
+            if (word.isBlank()) continue;
 
+            if (index != 0) {
+                String blank = s.substring(index, index + s.substring(index).indexOf(word));
+                answer += blank;
+                index += blank.length();
+            } else {
+                index += s.indexOf(word);
+                answer += s.substring(0, index);
+                System.out.println("answer = " + answer);
+            }
+
+            index += word.length();
+            String newWord = changeWord(word);
+            answer += newWord;
+        }
+
+        answer += s.substring(index);
+
+        return answer;
+    }
+
+    private static String changeWord(String word) {
+        String newWord = "";
+
+        for(int i=0; i<word.length(); i++) {
+            if (i % 2 == 0) {
+                newWord += word.substring(i, i+1).toUpperCase();
+            } else {
+                newWord += word.substring(i, i+1).toLowerCase();
+            }
+        }
+        return newWord;
     }
 
 }
