9a5bcd3e573b5e1a33e181d1678568e4608d16ad
ys200209 <lys200209@naver.com>
2022-09-16 15:30:41 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1062.java
+++ BOJ/Brute_Force/Main11_1062.java
@@ -4,32 +4,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main11_1062 {
-    static int answer=0;
+    static int answer=(int)1e9;
+    static List<NameCard> cards = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        System.out.println(solution(new int[]{1,3,2,5,4}, 9)); // 3
-//        System.out.println(solution(new int[]{2,2,3,3}, 10)); // 4
+        System.out.println(solution(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}})); // 4000
+//        System.out.println(solution(new int[][]{{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}})); // 120
 
     }
 
-    public static int solution(int[] d, int budget) {
-        Arrays.sort(d);
-        System.out.println(Arrays.toString(d));
-        DFS(0, 0, d, budget);
+    public static int solution(int[][] sizes) {
+        /*int w=0;
+        int h=0;
+        for(int i=0; i<sizes.length; i++) {
+            w = Math.max(w, sizes[i][0]);
+            h = Math.max(h, sizes[i][1]);
+            cards.add(new NameCard(sizes[i][0], sizes[i][1], false));
+        }
+        answer = w * h;
 
-        return answer;
-    }
+        DFS(0);*/
 
-    private static void DFS(int index, int count, int[] d, int budget) {
-        if (count > answer) {
-            answer = count;
+        int w = 0;
+        int h = 0;
+        for (int[] size : sizes) {
+            if (answer == 0) {
+                w = size[0];
+                h = size[1];
+                answer = w*h;
+            } else {
+                // 뒤집은게 더 작은 사각형이라면
+                if (Math.max(w, size[0]) * Math.max(h, size[1]) > Math.max(w, size[1]) * Math.max(h, size[0])) {
+                    w = Math.max(w, size[1]);
+                    h = Math.max(h, size[0]);
+                } else {
+                    w = Math.max(w, size[0]);
+                    h = Math.max(h, size[1]);
+                }
+            }
         }
 
-        for(int i=index; i<d.length; i++) {
-            if (budget >= d[i]) {
-                DFS(i+1, count+1, d, budget-d[i]);
-                break;
-            } else return;
+        return w*h;
+    }
+
+    /*private static void DFS(int index) {
+        answer = Math.min(answer, getSize());
+
+        for(int i=index; i<cards.size(); i++) {
+            cards.get(i).isRotate = true;
+            DFS(i+1);
+            cards.get(i).isRotate = false;
+        }
+    }
+
+    private static int getSize() {
+        int w=0, h=0;
+
+        for (NameCard card : cards) {
+            w = Math.max(w, card.isRotate ? card.h : card.w);
+            h = Math.max(h, card.isRotate ? card.w : card.h);
+        }
+        return w*h;
+    }*/
+
+    static class NameCard {
+        int w;
+        int h;
+
+        public NameCard(int w, int h) {
+            this.w = w;
+            this.h = h;
         }
     }
 
