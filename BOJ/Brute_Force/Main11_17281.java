ab0f01774c7d80a8563e7ff4f751dab906852133
ys200209 <lys200209@naver.com>
2022-09-17 16:15:20 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_17281.java
+++ BOJ/Brute_Force/Main11_17281.java
@@ -4,58 +4,104 @@ import java.io.*;
 import java.util.*;
 
 public class Main11_17281 {
-    static Map<String, Integer> map = new HashMap<>();
+    static Queue<Algorithm> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println(solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5})); // TCMA
+//        System.out.println(solution(10, 10, new int[][]{{10, 15, 2, 1, 2}, {20, 20, 3, 3, 4}})); // 15
+        System.out.println(solution(0, 0, new int[][]{{0,0,2,1,2},
+                {4,5,3,1,2}, {4,11,4,0,2}, {10,4,0,4,2}})); // 13
 
     }
 
-    public static String solution(String[] survey, int[] choices) {
-        String answer = "";
+    public static int solution(int alp, int cop, int[][] problems) {
+        Algorithm algorithm = new Algorithm(alp, cop, 0);
 
-        for(int i=0; i< survey.length; i++) {
-            String s = survey[i];
-            String[] split = s.split("");
-            String A = split[0];
-            String B = split[1];
-            int choice = choices[i];
-
-            MBTI(A, B, choice);
+        if (isSuccess(algorithm, problems)) {
+            return algorithm.time;
         }
-
-        answer += getMBTI("R", "T");
-        answer += getMBTI("C", "F");
-        answer += getMBTI("J", "M");
-        answer += getMBTI("A", "N");
-
-        return answer;
+        pq.offer(algorithm);
+//        int result = Study(problems);
+//        System.out.println("result = " + result);
+        return Study(problems);
     }
 
-    public static void MBTI(String A, String B, int choice) {
-        if (choice < 4) {
-            map.put(A, map.getOrDefault(A, 0) + (4 - choice));
-        } else if (choice > 4) {
-            map.put(B, map.getOrDefault(B, 0) + (choice - 4));
-        }
-    }
+    private static int Study(int[][] problems) {
+        int minTime = (int)1e9;
 
-    public static String getMBTI(String A, String B) {
-        if (map.get(A) == null && map.get(B) == null) {
-            if (A.charAt(0) > B.charAt(0)) return B;
-            else return A;
-        } else if (map.get(A) == null) {
-            return B;
-        } else if (map.get(B) == null) {
-            return A;
-        } else {
-            if (map.get(A) > map.get(B)) return A;
-            else if (map.get(A) == map.get(B)) {
-                if (A.charAt(0) > B.charAt(0)) return B;
-                else return A;
+        while(!pq.isEmpty()) {
+            Algorithm poll = pq.poll();
+            int alp = poll.alp;
+            int cop = poll.cop;
+            int time = poll.time;
+
+//            System.out.println("alp = " + alp + ", cop = " + cop + ", time = " + time);
+
+            if (minTime <= time) continue;
+
+            if (isSuccess(poll, problems)) {
+//                System.out.println("isSuccess()");
+                minTime = Math.min(minTime, time);
+//                System.out.println();
+                continue;
             }
-            else return B;
+
+            for(int i=0; i<problems.length; i++) {
+//                System.out.println("i = " + i);
+
+                int alpGap =  problems[i][0] - alp;
+                int copGap = problems[i][1] - cop;
+//                System.out.println("alpGap = " + alpGap);
+//                System.out.println("copGap = " + copGap);
+
+                if (alpGap > 0) pq.offer(new Algorithm(alp + alpGap, cop, time + alpGap));
+                if (copGap > 0) pq.offer(new Algorithm(alp, cop + copGap, time + copGap));
+
+                if (alp >= problems[i][0] && cop >= problems[i][1]) {
+//                    System.out.println("problem seq : " + i);
+
+                    /*int alpProblemCount = problems[i][2] == 0 ? -1 : problems[i][0] / problems[i][2];
+                    int copProblemCount = problems[i][3] == 0 ? -1 : problems[i][1] / problems[i][3];*/
+
+//                    System.out.println("problem count : " + alpProblemCount);
+
+                    pq.offer(new Algorithm(alp + problems[i][2], cop + problems[i][3], time + problems[i][4]));
+
+                    /*if (problems[i][0] - alp > 0 && problems[i][2] > 0) pq.offer(new Algorithm(alp + problems[i][2],
+                            cop + problems[i][3], time + problems[i][4]));
+
+                    if (problems[i][1] - cop > 0 && problems[i][3] > 0) pq.offer(new Algorithm(alp + problems[i][2],
+                            cop + problems[i][3], time + problems[i][4]));*/
+                }
+
+            }
+//            System.out.println("pq.size() : " + pq.size());
+        }
+        return minTime;
+    }
+
+    private static boolean isSuccess(Algorithm algorithm, int[][] problems) {
+        for(int i=0; i< problems.length; i++) {
+            if (algorithm.alp < problems[i][0] || algorithm.cop < problems[i][1]) return false;
+        }
+        return true;
+    }
+
+    static class Algorithm implements Comparable<Algorithm> {
+
+        int alp;
+        int cop;
+        int time;
+
+        public Algorithm(int alp, int cop, int time) {
+            this.alp = alp;
+            this.cop = cop;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Algorithm o) {
+            return this.time - o.time;
         }
     }
 
