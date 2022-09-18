54535a33d3621979af518eb84095ad654b511c7a
ys200209 <lys200209@naver.com>
2022-09-18 23:13:19 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_14889.java
+++ BOJ/Brute_Force/Main11_14889.java
@@ -1,38 +1,63 @@
+package BOJ.Brute_Force;
+
 import java.io.*;
 import java.util.*;
 
 public class Main11_14889 {
-    static int N, result = (int) 1e9;
-    static int a, b;
-    static int[][] map;
-    
+    static int N, result=(int)1e9;
+    static int[][] board;
+    static List<Integer> team = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        StringTokenizer st;
+        N =Integer.parseInt(br.readLine());
+        board = new int[N][N];
 
         for(int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine(), " ");
             int j=0;
             while(st.hasMoreTokens()) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
                 j++;
             }
         }
 
-        for(int i=0; i<N; i++) {
-            System.out.println(Arrays.toString(map[i]));
+        DFS(0, 0);
+
+        System.out.println(result);
+    }
+
+    private static void DFS(int index, int count) {
+        if (count == N/2) {
+            result = Math.min(result, getScore());
+            return;
         }
 
+        for(int i=index; i<N; i++) {
+            team.add(i);
+            DFS(i+1, count+1);
+            team.remove(count);
+        }
+
+    }
+
+    private static int getScore() {
+        int score1 = 0;
+        int score2 = 0;
+
         for(int i=0; i<N; i++) {
-            for(int j=i+1; j<N; j++) {
-                result = Math.min(result, map[i][j] + map[j][i]);
+            for(int j=0; j<N; j++) {
+                if (team.contains(i) && team.contains(j)) {
+                    score1 += board[i][j];
+                } else if (!team.contains(i) && !team.contains(j)) {
+                    score2 += board[i][j];
+                }
             }
         }
 
-        System.out.println("result = " + result);
-
+        return Math.abs(score1 - score2);
     }
 
 }
