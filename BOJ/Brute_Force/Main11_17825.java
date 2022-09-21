473b0d67d8ef79d76b4a4364b2ab9c985fbd44f1
ys200209 <lys200209@naver.com>
2022-09-22 00:50:26 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_17825.java
+++ BOJ/Brute_Force/Main11_17825.java
@@ -4,46 +4,22 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main11_17825 {
-    static int N, answer = 0;
-    static Stack stack = new Stack();
 
     public static void main(String[] args) throws IOException {
-        System.out.println(solution("[](){}")); // 3
-//        System.out.println(solution("}]()[{")); // 2
+        System.out.println(Arrays.toString(solution(3, 2, 5))); // [3,2,2,3]
     }
 
-    public static int solution(String s) {
-        String[] split = s.split("");
-        N = split.length;
-//        System.out.println("N = " + N);
-//        if (checkString(split, 0)) answer++;
+    public static int[] solution(int n, long left, long right) {
+        int size = (int)(right-left) + 1;
+        int[] answer = new int[size];
 
-        for(int i=0; i<N; i++) {
-            if (checkString(split, i)) answer++;
+        for(int i=0; i<size; i++) {
+            int x = (int)((i+left)/n);
+            int y = (int)((i+left)%n);
+            answer[i] = Math.max(x+1, y+1);
         }
 
         return answer;
-    }
-
-    private static boolean checkString(String[] split, int start) {
-//        System.out.println("---------------");
-        stack.clear();
-        int index;
-
-        for(int i=start; i<N+start; i++) {
-            index = i >= N ? i-N : i;
-//            System.out.println("index = " + index);
-            if (split[index].equals("(") || split[index].equals("{") || split[index].equals("[")) stack.push(split[index]);
-            else {
-                if (!stack.isEmpty() && split[index].equals("}") && stack.peek().equals("{")) stack.pop();
-                else if (!stack.isEmpty() && split[index].equals("]") && stack.peek().equals("[")) stack.pop();
-                else if (!stack.isEmpty() && split[index].equals(")") && stack.peek().equals("(")) stack.pop();
-                else return false;
-            }
-        }
-
-        if (stack.isEmpty()) return true;
-        else return false;
     }
 
 }
