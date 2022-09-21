69db3440765307a547c87c73578dc6f7ed3d5f35
ys200209 <lys200209@naver.com>
2022-09-21 17:32:22 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_17825.java
+++ BOJ/Brute_Force/Main11_17825.java
@@ -1,31 +1,49 @@
 package BOJ.Brute_Force;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main11_17825 {
+    static int N, answer = 0;
+    static Stack stack = new Stack();
 
     public static void main(String[] args) throws IOException {
-
-//        System.out.println(solution(5)); // 2
-        System.out.println(solution(5000)); // 5
-//        System.out.println(solution(10000)); //
-
-
+        System.out.println(solution("[](){}")); // 3
+//        System.out.println(solution("}]()[{")); // 2
     }
 
-    public static int solution(int n) {
-        return recursive(n);
+    public static int solution(String s) {
+        String[] split = s.split("");
+        N = split.length;
+//        System.out.println("N = " + N);
+//        if (checkString(split, 0)) answer++;
+
+        for(int i=0; i<N; i++) {
+            if (checkString(split, i)) answer++;
+        }
+
+        return answer;
     }
 
-    private static int recursive(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+    private static boolean checkString(String[] split, int start) {
+//        System.out.println("---------------");
+        stack.clear();
+        int index;
 
-        if (n % 2 == 0) return recursive(n/2);
-        else return recursive(n/2) + 1;
+        for(int i=start; i<N+start; i++) {
+            index = i >= N ? i-N : i;
+//            System.out.println("index = " + index);
+            if (split[index].equals("(") || split[index].equals("{") || split[index].equals("[")) stack.push(split[index]);
+            else {
+                if (!stack.isEmpty() && split[index].equals("}") && stack.peek().equals("{")) stack.pop();
+                else if (!stack.isEmpty() && split[index].equals("]") && stack.peek().equals("[")) stack.pop();
+                else if (!stack.isEmpty() && split[index].equals(")") && stack.peek().equals("(")) stack.pop();
+                else return false;
+            }
+        }
+
+        if (stack.isEmpty()) return true;
+        else return false;
     }
 
 }
