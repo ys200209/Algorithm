04d66bd57d1115218c593d0e3be75fd63588a803
ys200209 <lys200209@naver.com>
2022-09-22 21:59:36 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_17825.java
+++ BOJ/Brute_Force/Main11_17825.java
@@ -2,47 +2,75 @@ package BOJ.Brute_Force;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.function.IntFunction;
 
 public class Main11_17825 {
+    static Map<String, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-//        System.out.println(solution("...!@BaT#*..y.abcdefghijklm")); // bat.y.abcdefghi
-        System.out.println(solution("z-+.^.")); // z--
-//        System.out.println(solution("=.=")); // aaa
-//        System.out.println(solution("123_.def")); // 123_.def
-//        System.out.println(solution("abcdefghijklmn.p")); // abcdefghijklmn
+//        System.out.println(solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}));
+        // ["AC", "ACDE", "BCFG", "CDE"]
+
+//        System.out.println(solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}));
+        // ["ACD", "AD", "ADE", "CD", "XYZ"]
+
+        System.out.println(solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}));
+        // ["WX", "XY"]
     }
 
-    public static String solution(String new_id) {
-        String step1 = new_id.toLowerCase();
-        String step2 = "";
-        for(int i=0; i<step1.length(); i++) {
-            char c = step1.charAt(i);
-            if ((c-'a' >= 0 && c-'a'<26) || c-'-'==0 || c-'_'==0 || c-'.'==0 || (c-'0' <= 9 && c-'0'>=0)) {
-                if (step2.length() >= 1 && c-'.'==0 && step2.charAt(step2.length()-1)-'.'==0) continue; // step3 조건
-                step2 += c;
+    public static String[] solution(String[] orders, int[] course) {
+        for(int i=0; i<course.length; i++) {
+            for(int j=0; j<orders.length; j++) {
+                String[] menu = orders[j].split("");
+                Arrays.sort(menu);
+                combination(menu, "", 0, 0, course[i]);
             }
         }
 
-        String step4 = null;
-        String start = step2.substring(0, 1);
-        String end = step2.length() > 1 ? step2.substring(step2.length()-1) : "";
-        if (start.equals(".") && end.equals(".")) step4 = step2.substring(1, step2.length()-1);
-        else if (start.equals(".")) step4 = step2.substring(1);
-        else if (end.equals(".")) step4 = step2.substring(0, step2.length()-1);
-        else step4 = step2;
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (entry1, entry2) -> {
+            if (entry1.getKey().length() == entry2.getKey().length()) return entry2.getValue() - entry1.getValue();
+            else return entry1.getKey().length() - entry2.getKey().length();
+        });
 
-        String step5 = step4.trim().isBlank() ? "a" : step4;
+        List<String> answerList = new ArrayList<>();
+        int size = -1;
+        int max = -1;
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+            if (entry.getValue() == 1) continue; // 최소 2명 이상의 손님에게서 주문된 구성만 받아야 함
 
-        String step6 = step5.length() >= 16 ? step5.substring(14, 15).equals(".") ? step5.substring(0, 14) : step5.substring(0, 15) : step5;
-
-        String step7 = step6;
-        if (step7.length() <= 2) {
-            String last = step7.substring(step7.length()-1);
-            while(step7.length() < 3) step7 += last;
+            if (size == entry.getKey().length()) {
+                if (max <= entry.getValue()) {
+                    answerList.add(entry.getKey());
+                    max = entry.getValue();
+                }
+            } else {
+                size = entry.getKey().length();
+                max = entry.getValue();
+                answerList.add(entry.getKey());
+            }
         }
 
-        return step7;
+        Collections.sort(answerList);
+        String[] answer = answerList.toArray(new String[answerList.size()]);
+
+        System.out.println(Arrays.toString(answer));
+
+        return answer;
     }
+
+    private static void combination(String[] menu, String order, int index, int count, int max) {
+        if (count == max) {
+            map.put(order, map.getOrDefault(order, 0) + 1);
+            return;
+        }
+
+        for(int i=index; i<menu.length; i++) {
+            combination(menu, order+menu[i], i+1, count+1, max);
+        }
+
+    }
+
 
 }
