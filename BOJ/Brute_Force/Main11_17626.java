ab39f311d378a85e4ca767ac3d69d775d734c0bf
ys200209 <lys200209@naver.com>
2022-09-27 15:36:00 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_17626.java
+++ BOJ/Brute_Force/Main11_17626.java
@@ -13,20 +13,35 @@ public class Main11_17626 {
         N = Integer.parseInt(br.readLine());
         dp = new Integer[N+1];
 
-        for(int i=1; i<=N; i++) {
-            int pow = (int) Math.pow(i, 2);
+        /*dp[1] = 1;
+        for(int i=2; i*i<=N; i++) {
+            dp[i*i] = 1;
 
-            if (pow <= N) dp[pow] = 1;
-
-            for(int j=pow*2; j<=N; j+=pow) {
-                if (dp[j] == null) dp[j] = dp[j-pow] + 1;
-                else dp[j] = Math.min(dp[j], dp[j-pow] + 1);
+            for(int j=(i-1)*(i-1)+1; j<i*i; j++) {
+//                if (dp[])
+                int num = (i-1)*(i-1);
+                dp[j] = dp[num] + dp[j-num];
             }
+        }*/
 
-        }
+        recursive(N);
 
-        System.out.println(Arrays.toString(dp));
+//        System.out.println(Arrays.toString(dp));
 
+        System.out.println(dp[N]);
+
+        System.out.println(Math.sqrt(N));
+        System.out.println((int)Math.sqrt(N));
+//        System.out.println(Math.sqrt(N) % 1);
+
+
+    }
+
+    private static int recursive(int n) {
+        int sqrt = (int) Math.sqrt(n);
+
+        if (Math.sqrt(n) % 1 == 0) return dp[n] = 1;
+        else return dp[n] = Math.min(recursive(sqrt*sqrt) + recursive(n-(sqrt*sqrt)), recursive(sqrt*sqrt)+1);
     }
 
 }
