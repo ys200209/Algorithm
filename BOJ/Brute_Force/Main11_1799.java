6b8b02c7be549330df45d5ea0a045a37b6b0ab84
ys200209 <lys200209@naver.com>
2022-09-28 17:56:04 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1799.java
+++ BOJ/Brute_Force/Main11_1799.java
@@ -1,59 +1,33 @@
 package BOJ.Brute_Force;
 
+import java.util.*;
+
 public class Main11_1799 {
-    static int NUMBER=0;
 
     public static void main(String[] args) {
-
-        System.out.println(solution(2, 4, 2, 1)); // "0111"
-
-//        System.out.println(solution(16, 17, 2, 1)); // "0111"
-
-
+        System.out.println(solution(new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}})); // 16
     }
 
-    public static String solution(int n, int t, int m, int p) {
-        String answer = "";
+    public static int solution(int[][] land) {
+        int answer = 0;
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("0");
-
-        int i=1;
-        while(sb.length() < t*m) {
-            sb.append(game(n, i));
-            i++;
+        int[][] dp = new int[land.length][4];
+        for(int i=0; i<4; i++) {
+            dp[0][i] = land[0][i];
         }
 
-//        System.out.println("sb = " + sb);
-
-        String[] split = sb.toString().split("");
-        for(i=1; i<=split.length; i++) {
-            if (i%m == p%m) answer += split[i-1];
-
-            if (answer.length() == t) break;
+        for(int i=1; i<land.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][1], Math.max(dp[i-1][2], dp[i-1][3])) + land[i][0];
+            dp[i][1] = Math.max(dp[i-1][0], Math.max(dp[i-1][2], dp[i-1][3])) + land[i][1];
+            dp[i][2] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][3])) + land[i][2];
+            dp[i][3] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2])) + land[i][3];
         }
 
+        for(int i=0; i<4; i++) {
+            answer = Math.max(answer, dp[land.length-1][i]);
+        }
 
         return answer;
-    }
-
-    private static String game(int n, int number) { 
-        StringBuilder sb = new StringBuilder();
-
-        while(number > 0) {
-            int anInt = number%n;
-//            System.out.println("anInt = " + anInt);
-            if (anInt >= 10) sb.insert(0, (char)('A' + (anInt-10)));
-            else sb.insert(0, anInt);
-
-            number /= n;
-//            System.out.println("(prime) sb = " + sb);
-        }
-
-
-//        if (anInt >= 10) return (char)('A' + (anInt-10));
-//        else return sb.toString().charAt(0);
-        return sb.toString();
     }
 
 }
