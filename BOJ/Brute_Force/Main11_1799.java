97cf309e756a15fde6e8e198213b8ce85a97eddc
ys200209 <lys200209@naver.com>
2022-09-29 00:48:37 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1799.java
+++ BOJ/Brute_Force/Main11_1799.java
@@ -3,38 +3,60 @@ package BOJ.Brute_Force;
 import java.util.*;
 
 public class Main11_1799 {
-    static Map<String, Integer> map = new HashMap<>();
+    static Map<String, Integer> vector = new HashMap<>();
+    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    static int[] dy = {0, 0, -1, 1};
+    static Load[][] loads;
 
     public static void main(String[] args) {
-        System.out.println(solution("CBD", new String[]{"BACDE", "CBADF", "AECB", "BDA"})); // 2
+        System.out.println(solution("ULURRDLLU")); // 7
+//        System.out.println(solution("LULLLLLLU")); // 7
     }
 
-    public static int solution(String skill, String[] skill_trees) {
+    public static int solution(String dirs) {
         int answer = 0;
 
-        String[] split = skill.split("");
-        for(int i=0; i<split.length; i++) {
-            map.put(split[i], i+1);
+        vector.put("U", 0);
+        vector.put("D", 1);
+        vector.put("L", 2);
+        vector.put("R", 3);
+//        visited = new boolean[11][11];
+        loads = new Load[11][11];
+        for(int i=0; i<11; i++) {
+            for(int j=0; j<11; j++) {
+                loads[i][j] = new Load(i, j, new HashSet<>());
+            }
         }
 
-        for(int i=0; i<skill_trees.length; i++) {
-            split = skill_trees[i].split("");
-            if (checkSkill(split)) answer++;
+        String[] split = dirs.split("");
+        int x = 5;
+        int y = 5;
+        for (String s : split) {
+            int nx = x + dx[vector.get(s)];
+            int ny = y + dy[vector.get(s)];
+
+            if (nx < 0 || nx >= 11 || ny < 0 || ny >= 11) continue;
+
+            if (loads[x][y].visited.add(loads[nx][ny])) answer++;
+            loads[nx][ny].visited.add(loads[x][y]);
+
+            x = nx;
+            y = ny;
         }
 
         return answer;
     }
 
-    private static boolean checkSkill(String[] split) {
-        int number=0
-                ;
-        for(int i=0; i<split.length; i++) {
-            if (map.get(split[i]) == null) continue;
+    static class Load {
+        int x;
+        int y;
+        Set<Load> visited;
 
-            if (map.get(split[i]) == number+1) number++;
-            else return false;
+        public Load(int x, int y, Set<Load> visited) {
+            this.x = x;
+            this.y = y;
+            this.visited = visited;
         }
-        return true;
     }
 
 }
