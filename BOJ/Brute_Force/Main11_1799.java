40fc0897f46e90b9903b8dc18ccf4b32531aa178
ys200209 <lys200209@naver.com>
2022-09-30 21:18:11 +0900

    BOJ Brute_Force Algorithm

--- BOJ/Brute_Force/Main11_1799.java
+++ BOJ/Brute_Force/Main11_1799.java
@@ -1,62 +1,33 @@
 package BOJ.Brute_Force;
 
+import java.io.*;
 import java.util.*;
 
 public class Main11_1799 {
-    static Map<String, Integer> vector = new HashMap<>();
-    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-    static int[] dy = {0, 0, -1, 1};
-    static Load[][] loads;
 
-    public static void main(String[] args) {
-        System.out.println(solution("ULURRDLLU")); // 7
-//        System.out.println(solution("LULLLLLLU")); // 7
-    }
 
-    public static int solution(String dirs) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int answer = 0;
 
-        vector.put("U", 0);
-        vector.put("D", 1);
-        vector.put("L", 2);
-        vector.put("R", 3);
-//        visited = new boolean[11][11];
-        loads = new Load[11][11];
-        for(int i=0; i<11; i++) {
-            for(int j=0; j<11; j++) {
-                loads[i][j] = new Load(i, j, new HashSet<>());
-            }
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        String binaryString = Integer.toBinaryString(N);
+        System.out.println("Integer.toBinaryString(N) = " + binaryString);
+
+        int binary = Integer.parseInt(binaryString, 2);
+        System.out.println("binary = " + binary);
+
+        while(Integer.bitCount(N) > K) {
+            answer++;
+
+            N++;
         }
 
-        String[] split = dirs.split("");
-        int x = 5;
-        int y = 5;
-        for (String s : split) {
-            int nx = x + dx[vector.get(s)];
-            int ny = y + dy[vector.get(s)];
+        System.out.println("answer = " + answer);
 
-            if (nx < 0 || nx >= 11 || ny < 0 || ny >= 11) continue;
-
-            if (loads[x][y].visited.add(loads[nx][ny])) answer++;
-            loads[nx][ny].visited.add(loads[x][y]);
-
-            x = nx;
-            y = ny;
-        }
-
-        return answer;
     }
-
-    static class Load {
-        int x;
-        int y;
-        Set<Load> visited;
-
-        public Load(int x, int y, Set<Load> visited) {
-            this.x = x;
-            this.y = y;
-            this.visited = visited;
-        }
-    }
-
 }
