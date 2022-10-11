fc5e52686c4534deda80a9aefdcf3600d0f3c193
ys200209 <lys200209@naver.com>
2022-10-11 21:12:13 +0900

    BOJ String Algorithm

---
+++ BOJ/String_Algorithm/Main7_2743.java
@@ -0,0 +1,12 @@
+package BOJ.String_Algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class Main7_2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
